6082d35139f082b087a6c1eceb0ba086991b939b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-01-25 23:56:37 -0500

    Add files via upload

---
+++ php/orm/resources/Keychain.php
@@ -0,0 +1,64 @@
+<?php
+
+class Keychain
+{
+//PRIVATE VARS
+	private $protocol;
+	private $domainName;
+	private $extraPaths;
+	
+	private $hostPointer;
+	private $hostUsername;
+	private $hostPassword;
+	private $databaseName;
+
+//FACTORY
+	public function __construct() {
+		if(getenv("Openshift") == 1){
+			$this->hostPointer = getenv("CATERPILLARSV2_SERVICE_HOST");
+			$this->hostUsername = getenv("HOST_USERNAME");
+			$this->hostPassword = getenv("HOST_PASSWORD");
+			$this->databaseName = getenv("DATABASE_NAME");
+			
+			$this->protocol = "https://";
+			$this->domainName =  "caterpillarscount.unc.edu";
+			$this->extraPaths = "";
+		}
+		else{
+			require_once("GODADDY_KEYS.php");
+			$dbconnCreds = getDatabaseConnectionCredentials();
+			$this->hostPointer = $dbconnCreds[0];
+			$this->hostUsername = $dbconnCreds[1];
+			$this->hostPassword = $dbconnCreds[2];
+			$this->databaseName = $dbconnCreds[3];
+			
+			$pathComponents = getPathComponents();
+			$this->protocol = $pathComponents[0];
+			$this->domainName = $pathComponents[1];
+			$this->extraPaths = $pathComponents[2];
+		}
+	}
+
+
+//GETTERS
+	public function getDatabaseConnection(){
+		return mysqli_connect($this->hostPointer, $this->hostUsername, $this->hostPassword, $this->databaseName);
+	}
+	
+	public function getProtocol{
+		return $this->protocol;
+	}
+	
+	public function getDomain(){
+		return $this->domainName;
+	}
+	
+	public function getExtraPaths(){
+		return $this->extraPaths;
+	}
+	
+	public function getRoot(){
+		return $this->protocol . $this->domainName . $this->extraPaths;
+	}
+}		
+?>
