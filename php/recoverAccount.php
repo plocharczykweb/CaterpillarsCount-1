4c512a6e0a9c12cb6ad8d8d816b3832d0ac09987
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-01-29 11:49:56 -0500

    Update recoverAccount.php

--- php/recoverAccount.php
+++ php/recoverAccount.php
@@ -4,7 +4,7 @@
 	$email = $_GET["email"];
 	$user = User::findByEmail($email);
 	
-	if(get_class($user) == "User"){
+	if(!is_null($user) && get_class($user) == "User"){
 		if($user->recoverPassword()){
 			die("true");
 		}
