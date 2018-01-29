8bce7b1913dd5aae33a720924579bf5372d36cdb
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-01-29 11:44:29 -0500

    Update autoLogIn.php

--- php/autoLogIn.php
+++ php/autoLogIn.php
@@ -1,7 +1,7 @@
 <?php
 	require_once("orm/User.php");
 	$user = User::findBySignInKey($_GET["email"], $_GET["salt"]);
-	if(get_class($user) == "User"){
+	if(!is_null($user) && get_class($user) == "User"){
 		die("true");
 	}
 	die("false");
