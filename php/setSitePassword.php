669244ec735f46e3c86f3c4ae19e75f4035a4459
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-01-30 17:03:09 -0500

    Update setSitePassword.php

--- php/setSitePassword.php
+++ php/setSitePassword.php
@@ -8,9 +8,9 @@
 	$newPassword = $_GET["newPassword"];
 	
 	$user = User::findBySignInKey($email, $salt);
-	if(!is_null($user) && get_class($user) == "User"){
+	if(is_object($user) && get_class($user) == "User"){
 		$site = Site::findByName($siteName);
-		if(!is_null($site) && get_class($site) == "Site"){
+		if(is_object($site) && get_class($site) == "Site"){
 			if($site->getCreator()->getEmail() == $email){
 				if($site->passwordIsCorrect($newPassword)){
 					die("false|That is already " . $siteName . "'s password.");
