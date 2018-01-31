fc8b2fdc2ad159f8e4502aa5a95d0e0e75864d44
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-01-31 16:46:25 -0500

    Update autoLogIn.php

--- php/autoLogIn.php
+++ php/autoLogIn.php
@@ -1,4 +1,6 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+	
 	require_once("orm/User.php");
 	$user = User::findBySignInKey($_GET["email"], $_GET["salt"]);
 	if(is_object($user) && get_class($user) == "User"){
