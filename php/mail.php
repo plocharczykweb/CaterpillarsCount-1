8f754d41619bcfa8fef241bddab45a3cf07d7f72
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-02 14:26:23 -0500

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,25 +1,15 @@
 <?php
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\Exception;
-
-//Load composer's autoloader
-require 'vendor/autoload.php';
-
-//PHPMailer Object
+//require 'vendor/autoload.php';
 $mail = new PHPMailer;
 $mail->IsSMTP();
 $mail->Host = 'relay.unc.edu';  // UNC's SMTP relay server
 
-//From email address and name
+//From
 $mail->From = "caterpillarscount@gmail.com";
-$mail->FromName = "Caterpillars Count";
+$mail->FromName = "Caterpillars Count!";
 
-//To address and name
-//$mail->addAddress("recepient1@example.com", "Recepient Name");
-$mail->addAddress("plocharczykweb@gmail.com"); //Recipient name is optional
-
-//Address to which recipient will reply
-//$mail->addReplyTo("caterpillarscount@gmail.com", "Reply");
+//To
+$mail->addAddress("plocharczykweb@gmail.com"); //or $mail->addAddress("recepient1@example.com", "Recepient Name");
 
 //CC and BCC
 //$mail->addCC("cc@example.com");
@@ -32,12 +22,8 @@ $mail->Subject = "Test phpmailer";
 $mail->Body = "<i>Mail body in HTML</i>";
 $mail->AltBody = "This is the plain text version of the email content";
 
-if(!$mail->send()) 
-{
-    echo "Mailer Error: " . $mail->ErrorInfo;
+if($mail->send()){
+    die("Message has been sent successfully");
 } 
-else 
-{
-    echo "Message has been sent successfully";
-}
+die("Mailer Error: " . $mail->ErrorInfo);
 ?>
