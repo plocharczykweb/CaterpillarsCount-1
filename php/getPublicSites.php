3d363c0e98133e7a736c27b3472f74fa5852d803
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-03 13:56:41 -0500

    finished getPublicSites.php

--- php/getPublicSites.php
+++ php/getPublicSites.php
@@ -2,5 +2,15 @@
   require_once("orm/Site.php");
   
   $publicSites = Site::findAllPublicSites();
-  die(json_encode($publicSites));
+	$publicSitesArray = array();
+	for($i = 0; $i < count($publicSites); $i++){
+		$publicSitesArray[$i] = array(
+			"creatorEmail" => $publicSites[$i]->getCreator()->getEmail(),
+			"siteName" => $publicSites[$i]->getName(),
+			"description" => $publicSites[$i]->getDescription(),
+			"location" => $publicSites[$i]->getLocation(),
+			"plantCount" => count($publicSites[$i]->getPlants()),
+		);
+	}
+	die(json_encode($publicSitesArray));
 ?>
