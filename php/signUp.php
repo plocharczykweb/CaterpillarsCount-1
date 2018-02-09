e2d91ee6712dfd13ad4ac858d95d57c28324e66b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-09 13:46:45 -0500

    Added first name and last name to user sign up API

--- php/signUp.php
+++ php/signUp.php
@@ -2,10 +2,12 @@
 	header('Access-Control-Allow-Origin: *');
 	
 	require_once("orm/User.php");
+	$firstName = $_GET["firstName"];
+	$lastName = $_GET["lastName"];
 	$email = $_GET["email"];
 	$password = $_GET["password"];
 	
-	$newUser = User::create($email, $password);
+	$newUser = User::create($firstName, $lastName, $email, $password);
 	if(is_object($newUser) && get_class($newUser) == "User"){
 		$userid = intval($newUser->getID());
 		User::sendEmailVerificationCodeToUser($userid);
