3753db507bb594e1c57092d30984e78612909ef2
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-13 16:36:01 -0500

    return site name and region instead of ID

--- php/getPlantsBySite.php
+++ php/getPlantsBySite.php
@@ -10,19 +10,19 @@
 	
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    $site = Site::findByID($siteID);
-    if(is_object($site) && in_array($site, $user->getSites())){
-      $plants = $site->getPlants();
-      $circles = array();
-      for($i = 0; $i < ceil(count($plants)/5); $i++){
-        $circles[$i] = array(($i + 1), array());
-      }
-      for($i = 0; $i < count($plants); $i++){
-        $circles[($plants[$i]->getCircle() - 1)][1][] = array($plants[$i]->getOrientation(), $plants[$i]->getCode(), $plants[$i]->getSpecies());
-      }
-      die("true|" . json_encode(array($siteID, $circles)));
-    }
-    die("false|You do not have access to this site.");
+	    $site = Site::findByID($siteID);
+	    if(is_object($site) && in_array($site, $user->getSites())){
+	      $plants = $site->getPlants();
+	      $circles = array();
+	      for($i = 0; $i < ceil(count($plants)/5); $i++){
+		$circles[$i] = array(($i + 1), array());
+	      }
+	      for($i = 0; $i < count($plants); $i++){
+		$circles[($plants[$i]->getCircle() - 1)][1][] = array($plants[$i]->getOrientation(), $plants[$i]->getCode(), $plants[$i]->getSpecies());
+	      }
+	      die("true|" . json_encode(array($site->getName() . " (" . $site->getRegion() . ")", $circles)));
+	    }
+	    die("false|You do not have access to this site.");
 	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
