4ae4d9d552348a01f28feccbdc7b2e0c784a5e49
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-19 17:35:35 -0500

    Create addCircle.php

---
+++ php/temp/addCircle.php
@@ -0,0 +1,18 @@
+<?php
+	require_once('../orm/User.php');
+	require_once('../orm/Site.php');
+	
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+	$siteID = $_GET["siteID"];
+	
+	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User" && $user->getID() == 25){
+		$site = Site::findByID($siteID);
+		if(is_object($site) && get_class($site) == "Site"){
+			$site->addCircle();
+		}
+		die("ID does not correpond to a site.");
+	}
+	die("You are not logged in as a developer.");
+?>
