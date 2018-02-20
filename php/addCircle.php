bba658b17350089e5ab631a00a545f681ce76177
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-20 12:13:37 -0500

    On success, return new plants

--- php/addCircle.php
+++ php/addCircle.php
@@ -6,21 +6,28 @@
 	$salt = $_GET["salt"];
 	$samplePlantCode = $_GET["samplePlantCode"];
   
-  $plant = Plant::findByCode($samplePlantCode);
-  if(is_object($plant) && get_class($plant) == "Plant"){
-    $site = $plant->getSite();
-    if(is_object($site) && get_class($site) == "Site"){
-      $user = User::findBySignInKey($email, $salt);
-	    if(is_object($user) && get_class($user) == "User"){
-        if(in_array($site, $user->getSites())){
-          $site->addCircle();
-          die("true|");
-        }
-        die("false|You do not have permission to add a circle to this site.");
-      }
-      die("false|Your log in dissolved. Maybe you logged in on another device.");
-    }
-    die("false|The sample plant we extracted from this page does not correspond to an existing site.");
-  }
-  die("false|The sample plant code we extracted from this page did not correspond to an existing plant.");
+	$plant = Plant::findByCode($samplePlantCode);
+	if(is_object($plant) && get_class($plant) == "Plant"){
+		$site = $plant->getSite();
+		if(is_object($site) && get_class($site) == "Site"){
+			$user = User::findBySignInKey($email, $salt);
+			if(is_object($user) && get_class($user) == "User"){
+				if(in_array($site, $user->getSites())){
+					$newPlants = $site->addCircle();
+					$newPlants = array(
+						array($newPlants[0]->getCode(), $newPlants[0]->getSpecies()),
+						array($newPlants[1]->getCode(), $newPlants[1]->getSpecies()),
+						array($newPlants[2]->getCode(), $newPlants[2]->getSpecies()),
+						array($newPlants[3]->getCode(), $newPlants[3]->getSpecies()),
+						array($newPlants[4]->getCode(), $newPlants[4]->getSpecies()),
+					);
+					die("true|" . json_encode($newPlants));
+				}
+				die("false|You do not have permission to add a circle to this site.");
+			}
+			die("false|Your log in dissolved. Maybe you logged in on another device.");
+		}
+		die("false|The sample plant we extracted from this page does not correspond to an existing site.");
+	}
+	die("false|The sample plant code we extracted from this page did not correspond to an existing plant.");
 ?>
