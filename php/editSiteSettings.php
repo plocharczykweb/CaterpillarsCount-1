f0b0365f2e2898da5a129e26e1b3bb4395da07d6
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-27 16:08:46 -0500

    Create editSiteSettings.php

---
+++ php/editSiteSettings.php
@@ -0,0 +1,64 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	
+	require_once('orm/User.php');
+	
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+	$siteName = $_GET["siteName"];
+	$description = $_GET["description"];
+	$latitude = $_GET["latitude"];
+	$longitude = $_GET["longitude"];
+	$zoom = $_GET["zoom"];
+	$plantCount = intval($_GET["plantCount"]);
+	$sitePassword = $_GET["sitePassword"];
+	$public = $_GET["public"];
+	
+	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+		//make sure plant count is valid
+		if($plantCount % 5 != 0){
+			die("false|The number of plants you will survey must be a multiple of 5. ");
+		}
+		
+		//get region from lat/long
+		//Max of 2,500 free requests per day, calculated as the sum of client-side and server-side queries.
+		//Max of 50 requests per second, calculated as the sum of client-side and server-side queries.
+		$KEY = "AIzaSyC66haLntB413i6pkgSCXl3wpbrS4SPEx4";
+		$arr = json_decode(file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?latlng=" . $latitude . "," . $longitude . "&key=" . $KEY), true);
+		$country = "";
+		$region = "";
+		$addressComponents = $arr["results"][0]["address_components"];
+		for($i = 0; $i < count($addressComponents); $i++){
+			if(in_array("country", $addressComponents[$i]["types"])){
+				$country = $addressComponents[$i]["short_name"];
+			}
+			else if(in_array("administrative_area_level_1", $addressComponents[$i]["types"])){
+				$region = $addressComponents[$i]["short_name"];
+			}
+		}
+		
+		$finalRegion = $country;
+		if($country == "US" || $country == "CA"){
+			$finalRegion = $region;
+		}
+		
+		//create site
+		$site = $user->createSite($siteName, $description, $latitude, $longitude, $zoom, $finalRegion, $sitePassword, $public);
+		
+		//output errors if there are any
+		if(!is_object($site) || get_class($site) != "Site"){
+			die("false|" . $site);
+		}
+		
+		//if error free, create the plants for the site
+		for($i = 0; $i < ($plantCount / 5); $i++){
+			$site->addCircle();
+		}
+		//and email the creator
+		$site->emailPlantCodesToCreator();
+		die("true");
+		
+	}
+	die("false|Your log in dissolved. Maybe you logged in on another device.");
+?>
