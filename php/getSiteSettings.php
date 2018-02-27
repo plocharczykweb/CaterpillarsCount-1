7978664f7d1292e0560ccabfe82eb75f7beb9752
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-02-27 15:04:49 -0500

    Fixed indentation and syntax error

--- php/getSiteSettings.php
+++ php/getSiteSettings.php
@@ -1,27 +1,26 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
-	
+
 	require_once('orm/User.php');
 	require_once('orm/Site.php');
-	
-  $siteID = $_GET["siteID"];
+
+	$siteID = $_GET["siteID"];
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
-	
+
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
 		$sites = $user->getSites();
-    $site = Site::findByID($siteID);
-    if(is_object($site) && get_class($site) == "Site" && in_array($site, $sites)){
-      $siteArray = array(
-          "name" => $site->getName(),
-          "description" => $site->getDescription(),
-          "openToPublic" => $site->getOpenToPublic(),
-        );
-      }
-		  die("true|" . json_encode($sitesArray));
-    }
-    die("false|You do not have permission to edit this site.");
+		$site = Site::findByID($siteID);
+		if(is_object($site) && get_class($site) == "Site" && in_array($site, $sites)){
+			$siteArray = array(
+			"name" => $site->getName(),
+			"description" => $site->getDescription(),
+			"openToPublic" => $site->getOpenToPublic(),
+			);
+			die("true|" . json_encode($sitesArray));
+		}
+		die("false|You do not have permission to edit this site.");
 	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
