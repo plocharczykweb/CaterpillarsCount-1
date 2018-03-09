f03e7a7f979d2a7d15d01ee1ca3e58899b0c9a0b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-03-09 11:21:24 -0500

    email tags to user if they have permission for the site

--- php/emailTags.php
+++ php/emailTags.php
@@ -1,4 +1,18 @@
 <?php
+  require_once("orm/User.php");
   require_once("orm/Site.php");
-  sendPrintTagsEmailToCreator();
+
+  $email = $_GET["email"];
+	$salt = $_GET["salt"];
+  $siteID = $_GET["siteID"];
+	
+	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+    $site = Site::findByID($siteID);
+		if(is_object($site) && get_class($site) == "Site" && $site->sendPrintTagsEmailTo($user)){
+      die("true|");
+    }
+    die("false|You do not have permission to request tags for this site.");
+  }
+  die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
