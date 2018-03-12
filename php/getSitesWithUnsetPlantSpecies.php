8ef9c633c96ab8786a57ba639f8ea721553459c3
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-03-12 11:48:55 -0400

    return string of site names that have at least one unset plant species

--- php/getSitesWithUnsetPlantSpecies.php
+++ php/getSitesWithUnsetPlantSpecies.php
@@ -1,1 +1,32 @@
-
+<?php
+	header('Access-Control-Allow-Origin: *');
+	
+	require_once('orm/User.php');
+	
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+	
+	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+		$sites = $user->getSites();
+    $sitesWithUnsetPlantSpecies = array();
+    for($i = 0; $i < count($sites); $i++){
+      $plants = $sites[$i]->getPlants();
+      for($j = 0; $j < count($plants); $j++){
+        if(plants[$j].getSpecies() == "N/A"){
+          $sitesWithUnsetPlantSpecies[] = '"' . $sites[$i]->getName() . '"';
+          break;
+        }
+      }
+    }
+    $numberOfSitesWithUnsetPlantSpecies = count($sitesWithUnsetPlantSpecies);
+    if($numberOfSitesWithUnsetPlantSpecies > 0){
+      $sitesWithUnsetPlantSpecies = join(", ", $sitesWithUnsetPlantSpecies);
+      if($numberOfSitesWithUnsetPlantSpecies > 1){
+        $sitesWithUnsetPlantSpecies = substr_replace($sitesWithUnsetPlantSpecies, " and", strrpos($sitesWithUnsetPlantSpecies, ","), 1);
+      }
+      die("true|" . $sitesWithUnsetPlantSpecies);
+    }
+	}
+	die("false|");
+?>
