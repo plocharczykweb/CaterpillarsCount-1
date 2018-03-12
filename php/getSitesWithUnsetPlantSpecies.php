222bbbf33af15c1de640da7dd68c31f19f75a500
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-03-12 11:50:19 -0400

    fixed indentation

--- php/getSitesWithUnsetPlantSpecies.php
+++ php/getSitesWithUnsetPlantSpecies.php
@@ -9,24 +9,24 @@
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
 		$sites = $user->getSites();
-    $sitesWithUnsetPlantSpecies = array();
-    for($i = 0; $i < count($sites); $i++){
-      $plants = $sites[$i]->getPlants();
-      for($j = 0; $j < count($plants); $j++){
-        if(plants[$j].getSpecies() == "N/A"){
-          $sitesWithUnsetPlantSpecies[] = '"' . $sites[$i]->getName() . '"';
-          break;
-        }
-      }
-    }
-    $numberOfSitesWithUnsetPlantSpecies = count($sitesWithUnsetPlantSpecies);
-    if($numberOfSitesWithUnsetPlantSpecies > 0){
-      $sitesWithUnsetPlantSpecies = join(", ", $sitesWithUnsetPlantSpecies);
-      if($numberOfSitesWithUnsetPlantSpecies > 1){
-        $sitesWithUnsetPlantSpecies = substr_replace($sitesWithUnsetPlantSpecies, " and", strrpos($sitesWithUnsetPlantSpecies, ","), 1);
-      }
-      die("true|" . $sitesWithUnsetPlantSpecies);
-    }
+    		$sitesWithUnsetPlantSpecies = array();
+    		for($i = 0; $i < count($sites); $i++){
+      			$plants = $sites[$i]->getPlants();
+      			for($j = 0; $j < count($plants); $j++){
+        			if(plants[$j].getSpecies() == "N/A"){
+          				$sitesWithUnsetPlantSpecies[] = '"' . $sites[$i]->getName() . '"';
+          				break;
+        			}
+      			}
+    		}
+    		$numberOfSitesWithUnsetPlantSpecies = count($sitesWithUnsetPlantSpecies);
+    		if($numberOfSitesWithUnsetPlantSpecies > 0){
+      			$sitesWithUnsetPlantSpecies = join(", ", $sitesWithUnsetPlantSpecies);
+      			if($numberOfSitesWithUnsetPlantSpecies > 1){
+        			$sitesWithUnsetPlantSpecies = substr_replace($sitesWithUnsetPlantSpecies, " and", strrpos($sitesWithUnsetPlantSpecies, ","), 1);
+      			}
+      			die("true|" . $sitesWithUnsetPlantSpecies);
+    		}
 	}
 	die("false|");
 ?>
