9c16e2590ab97d65a800c460f4a1d57c5034dacb
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-03-14 11:50:47 -0400

    fixed indentation

--- php/getManagers.php
+++ php/getManagers.php
@@ -4,17 +4,17 @@
 	require_once('orm/User.php');
 	require_once('orm/Site.php');
 	
-  $siteID = $_GET["siteID"];
+  	$siteID = $_GET["siteID"];
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
 	
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    $site = Site::findByID($siteID);
-    if(is_object($site) && get_class($site) == "Site" && $user->getID() == $site->getCreator()->getID()){
-      die("true|" . $site->getManagersArray());
-    }
-    die("false|You did not create this site, so you cannot oversee its management.");
-  }
-  die("false|Your log in dissolved. Maybe you logged in on another device.");
+    		$site = Site::findByID($siteID);
+    		if(is_object($site) && get_class($site) == "Site" && $user->getID() == $site->getCreator()->getID()){
+      			die("true|" . $site->getManagersArray());
+    		}
+    		die("false|You did not create this site, so you cannot oversee its management.");
+  	}
+  	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
