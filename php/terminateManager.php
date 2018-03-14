85abd745663f77a75400766ddb19a3ce2c19d422
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-03-14 12:05:24 -0400

    Create terminateManager.php

---
+++ php/terminateManager.php
@@ -0,0 +1,34 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+	
+	require_once('orm/User.php');
+	require_once('orm/Site.php');
+	
+  $siteID = $_GET["siteID"];
+  $managerID = $_GET["managerID"];
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+	
+	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+    $site = Site::findByID($siteID);
+    if(is_object($site) && get_class($site) == "Site" && $user->getID() == $site->getCreator()->getID()){
+      $manager = User::findByID($managerID);
+      if(is_object($manager) && get_class($manager) == "User"){
+        $siteManagers = $site->getManagersArray();
+        for($i = 0; $i < count($siteManagers); $i++){
+          if($siteManagers[$i]["id"] == $manager->getID()){
+            if($site->terminateManager($manager)){
+              die("true|");
+            }
+            die("false|You are the site creator, not a manager.You cannot terminate yourself.");
+          }
+        }
+        die("false|There is no need for termination here. " . $manager->getFullName() . " is not a manager of this site anyway.");
+      }
+      die("false|There is no Caterpillars Count! user associated with that email address.");
+    }
+    die("false|You did not create this site, so you cannot oversee its management.");
+  }
+  die("false|Your log in dissolved. Maybe you logged in on another device.");
+?>
