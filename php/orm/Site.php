4e4b7dc244916d7e64597e411aa6fc4d0fb88405
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-04-27 18:37:31 -0400

    Extracted ManagerRequest stuff

--- php/orm/Site.php
+++ php/orm/Site.php
@@ -166,16 +166,11 @@ class Site
 	}
 	
 	public static function findManagedSitesByManager($manager){
-		$dbconn = (new Keychain)->getDatabaseConnection();
-		$query = mysqli_query($dbconn, "SELECT `SiteFK` FROM `SiteManager` WHERE `UserFKOfManager`='" . $manager->getID() . "' AND `Approved`='1'");
-		mysqli_close($dbconn);
-		
+		$sites = $manager->getSites();
 		$sitesArray = array();
-		while($managerRow = mysqli_fetch_assoc($query)){
-			$siteFK = $managerRow["SiteFK"];
-			$site = self::findByID($siteFK);
-			if(is_object($site) && get_class($site) == "Site"){
-				array_push($sitesArray, $site);
+		for($i = 0; $i < count($sites); $i++){
+			if($sites[$i]->getCreator() != $manager){
+				$sitesArray[] = $sites[$i];
 			}
 		}
 		return $sitesArray;
@@ -289,31 +284,6 @@ class Site
 			return false;
 		}
 		return true;
-	}
-	
-	public function getManagersArray(){
-		$dbconn = (new Keychain)->getDatabaseConnection();
-		$query = mysqli_query($dbconn, "SELECT * FROM `SiteManager` WHERE `SiteFK`='" . $this->id . "'");
-		mysqli_close($dbconn);
-		
-		//LOOP THROUGH ALL MANAGERS AND CONSTRUCT AN ARRAY OF MANAGERS TO RETURN
-		$managersArray = array();
-		while($managerRow = mysqli_fetch_assoc($query)){
-			$userFKOfManager = intval($managerRow["UserFKOfManager"]);
-			$manager = User::findByID($userFKOfManager);
-			
-			if(is_object($manager) && get_class($manager) == "User"){
-				$managerArray = array(
-					"id" => $userFKOfManager,
-					"approved" => $managerRow["Approved"],
-					"fullName" => $manager->getFullName(),
-					"email" => $manager->getEmail(),
-				);
-				
-				array_push($managersArray, $managerArray);
-			}
-		}
-		return $managersArray;
 	}
 	
 //SETTERS
@@ -586,49 +556,6 @@ class Site
 			return true;
 		}
 		mysqli_close($dbconn);
-		return false;
-	}
-	
-	public function appointManager($manager){
-		if(is_object($manager) && get_class($manager) == "User" && $manager->getID() != $this->creator->getID()){
-			$dbconn = (new Keychain)->getDatabaseConnection();
-			$query = mysqli_query($dbconn, "SELECT * FROM `SiteManager` WHERE `UserFKOfManager`='" . $manager->getID() . "' AND `SiteFK`='" . $this->id . "' LIMIT 1");
-			if(mysqli_num_rows($query) == 0){
-				mysqli_query($dbconn, "INSERT INTO SiteManager (`UserFKOfManager`, `SiteFK`, `Approved`) VALUES ('" . $manager->getID() . "', '" . $this->id . "', '0')");
-				//$id = intval(mysqli_insert_id($dbconn));
-				$message = "<div style=\"text-align:center;border-radius:5px;padding:20px;font-family:'Segoe UI', Frutiger, 'Frutiger Linotype', 'Dejavu Sans', 'Helvetica Neue', Arial, sans-serif;\"><div style=\"text-align:left;color:#777;margin-bottom:40px;font-size:20px;\">" . $this->getCreator()->getFullName() . " would like you to be a manager of the \"" . $this->getName() . "\" site in " . $this->getRegion() . ". Please sign in to <a href='https://caterpillarscount.unc.edu/signIn'>caterpillarscount.unc.edu</a> using this email address (" . $manager->getEmail() . ") to approve or deny this request.</div><a href='https://caterpillarscount.unc.edu/signIn'><button style=\"border:0px none transparent;background:#fed136; border-radius:5px;padding:20px 40px;font-size:20px;color:#fff;font-family:'Segoe UI', Frutiger, 'Frutiger Linotype', 'Dejavu Sans', 'Helvetica Neue', Arial, sans-serif;font-weight:bold;cursor:pointer;\">SIGN IN NOW</button></a><div style=\"padding-top:40px;margin-top:40px;margin-left:-40px;margin-right:-40px;border-top:1px solid #eee;color:#bbb;font-size:14px;\"></div></div>";
-				email($manager->getEmail(), "New Caterpillars Count! site manager request!", $message);
-			}
-			else if(mysqli_fetch_assoc($query)["Approved"] == -1){
-				mysqli_query($dbconn, "UPDATE SiteManager SET `Approved`='0' WHERE `UserFKOfManager`='" . $manager->getID() . "' AND `SiteFK`='" . $this->id . "'");
-				$message = "<div style=\"text-align:center;border-radius:5px;padding:20px;font-family:'Segoe UI', Frutiger, 'Frutiger Linotype', 'Dejavu Sans', 'Helvetica Neue', Arial, sans-serif;\"><div style=\"text-align:left;color:#777;margin-bottom:40px;font-size:20px;\">" . $this->getCreator()->getFullName() . " would like you to reconsider being a manager of the \"" . $this->getName() . "\" site in " . $this->getRegion() . ". Please sign in to <a href='https://caterpillarscount.unc.edu/signIn'>caterpillarscount.unc.edu</a> using this email address (" . $manager->getEmail() . ") to approve or deny this request.</div><a href='https://caterpillarscount.unc.edu/signIn'><button style=\"border:0px none transparent;background:#fed136; border-radius:5px;padding:20px 40px;font-size:20px;color:#fff;font-family:'Segoe UI', Frutiger, 'Frutiger Linotype', 'Dejavu Sans', 'Helvetica Neue', Arial, sans-serif;font-weight:bold;cursor:pointer;\">SIGN IN NOW</button></a><div style=\"padding-top:40px;margin-top:40px;margin-left:-40px;margin-right:-40px;border-top:1px solid #eee;color:#bbb;font-size:14px;\"></div></div>";
-				email($manager->getEmail(), "New Caterpillars Count! site manager request!", $message);
-			}
-			mysqli_close($dbconn);
-			return true;
-		}
-		return false;
-	}
-	
-	public function terminateManager($manager){
-		if(is_object($manager) && get_class($manager) == "User" && $manager->getID() != $this->creator->getID()){
-			$dbconn = (new Keychain)->getDatabaseConnection();
-			$query = mysqli_query($dbconn, "SELECT `Approved` FROM `SiteManager` WHERE `UserFKOfManager`='" . $manager->getID() . "' AND `SiteFK`='" . $this->id . "' LIMIT 1");
-			if(mysqli_num_rows($query) > 0){
-				$approved = intval(mysqli_fetch_assoc($query)["Approved"]);
-				$query = mysqli_query($dbconn, "DELETE FROM `SiteManager` WHERE `UserFKOfManager`='" . $manager->getID() . "' AND `SiteFK`='" . $this->id . "'");
-				if($approved > -1){
-					$approvedMessageAddOn = "";
-					if($approved == 1){
-						$approvedMessageAddOn = " Thank you for the time you've dedicated to managing this site in the past.";
-					}
-					$message = "<div style=\"text-align:center;border-radius:5px;padding:20px;font-family:'Segoe UI', Frutiger, 'Frutiger Linotype', 'Dejavu Sans', 'Helvetica Neue', Arial, sans-serif;\"><div style=\"text-align:left;color:#777;margin-bottom:40px;font-size:20px;\">" . $this->getCreator()->getFullName() . " no longer requires your services as a manager of the \"" . $this->getName() . "\" Caterpillars Count! site in " . $this->getRegion() . "." . $approvedMessageAddOn . "</div><div style=\"padding-top:40px;margin-top:40px;margin-left:-40px;margin-right:-40px;border-top:1px solid #eee;color:#bbb;font-size:14px;\"></div></div>";
-					email($manager->getEmail(), "Your Caterpillars Count! \"" . $this->getName() . "\" managment services are no longer required.", $message);
-				}
-			}
-			mysqli_close($dbconn);
-			return true;
-		}
 		return false;
 	}
 }		
