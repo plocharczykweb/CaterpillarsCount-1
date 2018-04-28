9a9f126abb6af00773a69e0727f65d21bd9e9e0f
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-04-27 22:01:54 -0400

    updated to use ManagerRequest class methods

--- php/respondToManagerRequest.php
+++ php/respondToManagerRequest.php
@@ -1,29 +1,34 @@
 <?php
 	require_once('orm/Site.php');
+	require_once('orm/ManagerRequest.php');
+	
+	$managerRequestID = $_GET["managerRequestID"];
+  	$response = $_GET["response"];
+  	$email = $_GET["email"];
+  	$salt = $_GET["salt"];
 
-  $managerRequestID = $_GET["managerRequestID"];
-  $response = $_GET["response"];
-  $email = $_GET["email"];
-  $salt = $_GET["salt"];
-
-  $user = User::findBySignInKey($email, $salt);
+  	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    if($response == "approve"){
-      if($user->approveManagerRequest($managerRequestID)){
-        $dbconn = (new Keychain)->getDatabaseConnection();
-        $site = Site::findByID(mysqli_fetch_assoc(mysqli_query($dbconn, "SELECT `SiteFK` FROM `SiteManager` WHERE `ID`='" . $managerRequestID . "' LIMIT 1"))["SiteFK"]);
-		    mysqli_close($dbconn);
-        die("true|" . $site->getName());
-      }
-      die("You do not have permission to approve this site manager request.");
-    }
-    else if($response == "deny"){
-      if($user->denyManagerRequest($managerRequestID)){
-        die("true|");
-      }
-      die("You do not have permission to deny this site manager request.");
-    }
-    die("false|Invalid response.");
-  }
-  die("false|Your log in dissolved. Maybe you logged in on another device.");
+		$managerRequest = ManagerRequest::findByID($managerRequestID);
+		if(get_class($managerRequest) == "ManagerRequest"){
+			if($managerRequest->getManager() == $user){
+    				if($response == "approve"){
+      					if($managerRequest->setStatus("Approved")){
+						die("true|" . $managerRequest->getSite()->getName());
+					}
+					die("false|Could not approve request.");
+    				}
+    				else if($response == "deny"){
+      					if($managerRequest->setStatus("Denied")){
+						die("true|");
+					}
+					die("false|Could not deny request.");
+    				}
+    				die("false|Invalid response.");
+			}
+			die("false|You do not have permission to respond to this site manager request.");
+		}
+		die("false|Could not locate manager request in order to respond to it.");
+  	}
+  	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
