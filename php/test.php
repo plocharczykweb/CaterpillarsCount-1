e8521185cc98b566b3a958df80df6919ef947812
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-04-28 03:11:21 -0400

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,24 +1,24 @@
 <?php
 require_once('orm/User.php');
+require_once('orm/Site.php');
 require_once('orm/ManagerRequest.php');
+require_once('orm/resources/Keychain.php');
 
 $user = User::findByEmail("aaron@game103.net");
 echo $user->getFullName();
-echo count(ManagerRequest::findPendingManagerRequestsByManager($user));
-  $managerRequests = ManagerRequest::findPendingManagerRequestsByManager($user);
-		$requestsArray = array();
-		for($i = 0; $i < count($managerRequests); $i++){
-			$requestArray = array(
-				"id" => $managerRequests[$i]->getID(),
-				"requester" => $managerRequests[$i]->getSite()->getCreator()->getFullName(),
-				"siteName" => $managerRequests[$i]->getSite()->getName(),
-				"siteDescription" => $managerRequests[$i]->getSite()->getDescription(),
-				"siteCoordinates" => $managerRequests[$i]->getSite()->getLatitude() . ", " . $managerRequests[$i]->getSite()->getLongitude(),
-				"siteRegion" => $managerRequests[$i]->getSite()->getRegion(),
-				"siteOpenToPublic" => $managerRequests[$i]->getSite()->getOpenToPublic(),
-			);
+$dbconn = (new Keychain)->getDatabaseConnection();
+		$query = mysqli_query($dbconn, "SELECT * FROM `ManagerRequest` WHERE `UserFKOfManager`='" . $user->getID() . "' AND `Status`='Pending'");
+		mysqli_close($dbconn);
+		
+		$managerRequestsArray = array();
+		while($managerRequestRow = mysqli_fetch_assoc($query)){
+			$id = $managerRequestRow["ID"];
+			$manager = User::findByID($managerRequestRow["UserFKOfManager"]);
+			$site = Site::findByID($managerRequestRow["SiteFK"]);
+			$status = $managerRequestRow["Status"];
+			$managerRequest = new ManagerRequest($id, $manager, $site, $status);
 			
-			array_push($requestsArray, $requestArray);
+			array_push($managerRequestsArray, "");
 		}
-		die("true|" . json_encode($requestsArray));
+		echo count($managerRequestsArray);
   ?>
