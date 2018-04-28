7c014a0123bf18915f7f85e3a7e354fa9295d369
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-04-27 21:37:33 -0400

    Removed "Invalid site" error message

--- php/sendManagerRequest.php
+++ php/sendManagerRequest.php
@@ -13,38 +13,35 @@
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
     		$site = Site::findByID($siteID);
-    		if(is_object($site) && get_class($site) == "Site"){
-			if($user->getID() == $site->getCreator()->getID()){
-      				$manager = User::findByEmail($managerEmail);
-      				if(is_object($manager) && get_class($manager) == "User"){
-					$managerRequest = ManagerRequest::findByManagerAndSite($manager, $site);
-					if($managerRequest !== null){
-						if($managerRequest->getStatus() == "Pending"){
-							die("false|" . $manager->getFullName() . " has already received a request to be a manager for this site.");
-						}
-						else if($managerRequest->getStatus() == "Approved"){
-							die("false|" . $manager->getFullName() . " is already a manager of this site.");
-						}
+    		if(is_object($site) && get_class($site) == "Site" && $user->getID() == $site->getCreator()->getID()){
+      			$manager = User::findByEmail($managerEmail);
+      			if(is_object($manager) && get_class($manager) == "User"){
+				$managerRequest = ManagerRequest::findByManagerAndSite($manager, $site);
+				if($managerRequest !== null){
+					if($managerRequest->getStatus() == "Pending"){
+						die("false|" . $manager->getFullName() . " has already received a request to be a manager for this site.");
 					}
-					
-					$managerRequest = ManagerRequest::create($site, $manager);
-					
-					if(get_class($managerRequest) == "ManagerRequest"){
-						$managerRequestArray = array(
-							"id" => $managerRequest->getID(),
-							"status" => $managerRequest->getStatus(),
-							"fullName" => $manager->getFullName(),
-							"email" => $manager->getEmail(),
-						);
-          					die("true|" . json_encode($managerRequestArray));
-       					}
-        				die("false|" . trim($managerRequest));
-      				}
-      				die("false|There is no Caterpillars Count! user associated with that email address.");
-    			}
-    			die("false|You did not create this site, so you cannot oversee its management.");
-		}
-		die("false|Invalid site.");
+					else if($managerRequest->getStatus() == "Approved"){
+						die("false|" . $manager->getFullName() . " is already a manager of this site.");
+					}
+				}
+				
+				$managerRequest = ManagerRequest::create($site, $manager);
+				
+				if(get_class($managerRequest) == "ManagerRequest"){
+					$managerRequestArray = array(
+						"id" => $managerRequest->getID(),
+						"status" => $managerRequest->getStatus(),
+						"fullName" => $manager->getFullName(),
+						"email" => $manager->getEmail(),
+					);
+          				die("true|" . json_encode($managerRequestArray));
+       				}
+        			die("false|" . trim($managerRequest));
+      			}
+      			die("false|There is no Caterpillars Count! user associated with that email address.");
+    		}
+		die("false|You did not create this site, so you cannot oversee its management.");
 	}
   	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
