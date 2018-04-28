fed27944bf222d929e217e8521b8ad37e3a55fa3
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-04-27 23:13:18 -0400

    update to use ManagerRequest class methods

--- php/getManagerRequests.php
+++ php/getManagerRequests.php
@@ -8,7 +8,20 @@
 	
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-		die("true|" . json_encode($user->getPendingManagerRequests()));
+		$managerRequests = $user->getPendingManagerRequests();
+		$requestsArray = array();
+		for($i = 0; $i < count($managerRequests); $i++){
+			$requestsArray[] = array(
+				"id" => $managerRequests[$i]->getID(),
+				"requester" => $managerRequests[$i]->getSite()->getCreator()->getFullName(),
+				"siteName" => $managerRequests[$i]->getSite()->getName(),
+				"siteDescription" => $managerRequests[$i]->getSite()->getDescription(),
+				"siteCoordinates" => $managerRequests[$i]->getSite()->getLatitude() . ", " . $managerRequests[$i]->getSite()->getLatitude(),
+				"siteRegion" => $managerRequests[$i]->getSite()->getRegion(),
+				"siteOpenToPublic" => $managerRequests[$i]->getSite()->getOpenToPublic(),
+			);
+		}
+		die("true|" . json_encode($requestsArray));
 	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
