0710d3cc025c6045ea1707f6bab0c436920796c9
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-08 15:19:45 -0400

    customized error messages

--- php/signOutOfAllOtherDevices.php
+++ php/signOutOfAllOtherDevices.php
@@ -10,13 +10,13 @@
 	if(is_object($user) && get_class($user) == "User"){
 		$salt = $user->signOutOfAllOtherDevices($password);
 		
-		if($salt != false){
+		if($salt !== false){
 			die("success" . $salt);
 		}
-		die("Some of that info is incorrect.");//incorrect password
+		die("That is not the password for the " . $email . " account you are logged in with.");//incorrect password
 	}
 	else if(User::emailIsUnvalidated($email)){//check if email is unverified
 		die("Check your email to verify your account. Check spam if needed!");
 	}
-	die("Some of that info is incorrect.");//incorrect username
+	die("false|Your log in dissolved.");//incorrect email
 ?>
