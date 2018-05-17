4353e9be064632a2527bb725eca9e20605740d8c
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-17 14:07:43 -0400

    die survey data

--- php/getMySurveys.php
+++ php/getMySurveys.php
@@ -8,8 +8,32 @@
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
 		$surveys = Survey::findByUser($user);
+		$surveysArray = array();
 		for($i = 0; $i < count($surveys); $i++){
-			echo $surveys[$i]->getID() . "<br/>";
+			$arthropodSightings = $surveys[$i]->getArthropodSightings();
+			$arthropodSightingsArray = array();
+			$surveysArray[] = array(
+				"id" => $surveys[$i]->getID(),
+				"observerID" => $surveys[$i]->getObserver()->getID(),
+				"observerFullName" => $surveys[$i]->getObserver()->getFullName(),
+				"plantCode" => $surveys[$i]->getPlant()->getCode(),
+				"siteID" => $surveys[$i]->getPlant()->getSite()->getID(),
+				"siteName" => $surveys[$i]->getPlant()->getSite()->getName(),
+				"siteRegion" => $surveys[$i]->getPlant()->getSite()->getRegion(),
+				"siteCoordinates" => $surveys[$i]->getPlant()->getSite()->getLatitude() . "," . $surveys[$i]->getPlant()->getSite()->getLongitude(),
+				"localDate" => $surveys[$i]->getLocalDate(),
+				"localTime" => $surveys[$i]->getLocalTime(),
+				"observaionMethod" => $surveys[$i]->getObservationMethod(),
+				"notes" => $surveys[$i]->getNotes(),
+				"wetLeaves" => $surveys[$i]->getWetLeaves(),
+				"plantSpecies" => $surveys[$i]->getPlantSpecies(),
+				"numberOfLeaves" => $surveys[$i]->getNumberOfLeaves(),
+				"averageLeafLength" => $surveys[$i]->getAverageLeafLength(),
+				"herbivoryScore" => $surveys[$i]->getHerbivoryScore(),
+				"submittedThroughApp" => $surveys[$i]->getSubmittedThroughApp(),
+				"arthropodSightings" => $arthropodSightingsArray,
+			);
 		}
+		die(json_encode($surveysArray));
 	}
 ?>
