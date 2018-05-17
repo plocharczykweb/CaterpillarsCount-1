42451f59a9e070eb7eb55e5e333b5ffe91c1963e
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-17 11:38:01 -0400

    added require_once('orm/Survey.php');

--- php/getMySurveys.php
+++ php/getMySurveys.php
@@ -1,14 +1,15 @@
 <?php
-  require_once('orm/User.php');
+	require_once('orm/User.php');
+	require_once('orm/Survey.php');
 	
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
 	
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    $surveys = Survey::findByUser($user);
-    for($i = 0; $i < count($surveys); $i++){
-      echo $surveys[$i]->getID() . "<br/>";
-    }
-  }
+		$surveys = Survey::findByUser($user);
+		for($i = 0; $i < count($surveys); $i++){
+			echo $surveys[$i]->getID() . "<br/>";
+		}
+	}
 ?>
