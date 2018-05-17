51c1abfcad7ba6099f055362e0d8d3c0e64f3d2f
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-17 11:24:48 -0400

    Create getMySurveys.php

---
+++ php/getMySurveys.php
@@ -0,0 +1,14 @@
+<?php
+  require_once('orm/User.php');
+	
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+	
+	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+    $surveys = Survey::findByUser($user);
+    for($i = 0; $i < count($surveys); $i++){
+      echo $surveys[$i]->getID() . "<br/>";
+    }
+  }
+?>
