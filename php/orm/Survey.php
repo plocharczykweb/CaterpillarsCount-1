4bfc0e1d5be9e18bad1d6de9f1ffb560ee94769d
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-17 17:08:16 -0400

    added TODO item

--- php/orm/Survey.php
+++ php/orm/Survey.php
@@ -149,6 +149,7 @@ class Survey
 		//TODO: SiteFK column is not in Survey table.
 		$query = mysqli_query($dbconn, "SELECT Survey.* FROM `Survey` JOIN `Plant` ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK IN (" . join(",", $siteIDs) . ") OR Survey.UserFKOfObserver='" . $user->getID() . "' ORDER BY Survey.LocalDate DESC, Survey.LocalTime DESC, Survey.ID DESC LIMIT " . $start . ", " . $limit);
 		//$query = mysqli_query($dbconn, "SELECT Survey.* FROM `Survey` JOIN `Plant` ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK='" . $sites[$i]->getID() . "' AND Survey.UserFKOfObserver<>'" . $user->getID() . "'");
+		//TODO: check if query has ay results
 		while($surveyRow = mysqli_fetch_assoc($query)){
 			$id = $surveyRow["ID"];
 			$observer = User::findByID($surveyRow["UserFKOfObserver"]);
