d853be8bfbdb279dfee0717f470c30018260d46f
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-20 15:40:54 -0400

    don't let page == 1 act as page == "last" in findSurveysByUser

--- php/orm/Survey.php
+++ php/orm/Survey.php
@@ -148,7 +148,7 @@ class Survey
 		}
 		
 		$totalCount = intval(mysqli_fetch_assoc(mysqli_query($dbconn, "SELECT COUNT(*) AS `Count` FROM `Survey` JOIN `Plant` ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK IN (" . join(",", $siteIDs) . ") OR Survey.UserFKOfObserver='" . $user->getID() . "'"))["Count"]);
-		if($start == "last"){
+		if($start === "last"){
 			$start = $totalCount - ($totalCount % intval($limit));
 		}
 		$query = mysqli_query($dbconn, "SELECT Survey.* FROM `Survey` JOIN `Plant` ON Survey.PlantFK = Plant.ID WHERE Plant.SiteFK IN (" . join(",", $siteIDs) . ") OR Survey.UserFKOfObserver='" . $user->getID() . "' ORDER BY Survey.LocalDate DESC, Survey.LocalTime DESC, Survey.ID DESC LIMIT " . $start . ", " . $limit);
