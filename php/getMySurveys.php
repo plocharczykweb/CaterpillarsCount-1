8c749ebf2a7041da4832fadca0f8636d31797600
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-23 13:12:27 -0400

    adjusted for change in what findSurveysByUser returns

--- php/getMySurveys.php
+++ php/getMySurveys.php
@@ -15,7 +15,8 @@
 			$start = ((intval($page) - 1) * $PAGE_LENGTH);
 		}
 		$surveys = Survey::findSurveysByUser($user, $filters, $start, $PAGE_LENGTH);
-		$totalPages = ceil(count($surveys)/$PAGE_LENGTH);
+		$totalPages = ceil($surveys[0]/$PAGE_LENGTH);
+		$surveys = $surveys[1];
 		$surveysArray = array();
 		for($i = 0; $i < count($surveys); $i++){
 			if(is_object($surveys[$i]) && get_class($surveys[$i]) == "Survey"){
