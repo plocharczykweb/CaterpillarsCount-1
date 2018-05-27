c839932d135408e18623eb4c6934b5df6cd3f8b5
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-27 15:41:58 -0400

    added mysql query

--- php/getMySurveySites.php
+++ php/getMySurveySites.php
@@ -2,6 +2,7 @@
 	header('Access-Control-Allow-Origin: *');
 	
 	require_once('orm/User.php');
+	require_once('orm/Site.php');
 	
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
@@ -9,6 +10,18 @@
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
 		$sites = $user->getSites();
+		$siteIDs = array();
+		for($i = 0; $i < count($sites); $i++){
+			$siteIDs[] = $sites[$i]->getID();
+		}
+		$query = mysqli_query($dbconn, "SELECT Site.ID FROM `Survey` JOIN `Plant` ON Survey.PlantFK = Plant.ID JOIN `Site` ON Plant.SiteFK=Site.ID WHERE Survey.UserFKOfObserver='" . $user->getID() . "'");
+		while($siteRow = mysqli_fetch_assoc($query)){
+			$id = $siteRow["ID"];
+			if(!in_array($id, $siteIDs)){
+				$siteIDs[] = $id;
+				$sites[] = Site::findByID($id);
+			}
+		}
 		$sitesArray = array();
 		for($i = 0; $i < count($sites); $i++){
 			$sitesArray[$i] = array(
