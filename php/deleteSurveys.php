5cd6f64fadf91b3984dd3208eeab6bdc9c706c48
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-05-30 13:00:25 -0400

    added error messages

--- php/deleteSurveys.php
+++ php/deleteSurveys.php
@@ -8,25 +8,57 @@
 	
 	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    		$errors = "";
+    		$couldntIdentify = array();
+    		$noAuthority = array();
+		$successes = 0;
     		for($i = 0; $i < count($surveyIDs); $i++){
       			$survey = Survey::findByID($surveyIDs[$i]);
       			if(is_object($survey) && get_class($survey) == "Survey"){
         			if($survey->getObserver() === $user || in_array($survey->getPlant()->getSite(), $user->getSites(), true)){
           				$survey->permanentDelete();
+					$successes++;
         			}
         			else{
-          				$errors .= "You do not have the authority to delete the survey with id: " . $surveyIDs[$i] . ". ";
-        			}
+					$noAuthority[] = $surveyIDs[$i];
+				}
       			}
+			else{
+				$couldntIdentify[] = $surveyIDs[$i];
+			}
+    		}
+		$errors = "";
+    		if(count($couldntIdentify) > 0){
+			if(count($surveyIDs) < 2){
+				$errors .= "This survey does not exist and therefore cannot be deleted. ";
+			}
+			else if(count($couldntIdentify) == 1){
+				$errors .= "The survey with the ID " . $couldntIdentify[0] . " does not exist and therefore cannot be deleted. ";
+			}
       			else{
-        			$errors .= "Could not identify survey with ID: " . $surveyIDs[$i] . ". ";
-      			}
+				$errors .= "The surveys with the following IDs do not exist and therefore cannot be deleted: " . join(", ", $couldntIdentify) . ". ";
+			}
     		}
-    		if($errors == ""){
-      			die("true|");
-    		}
-		die("false|" . $errors);
+		if(count($noAuthority) > 0){
+			if(count($surveyIDs) < 2){
+				$errors .= "You do not have the authority to delete this survey. ";
+			}
+			else if(count($noAuthority) == 1){
+				$errors .= "You do not have the authority to delete the survey with the ID " . $noAuthority[0] . ". ";
+			}
+      			else{
+				$errors .= "You do not have the authority to delete the surveys with the following IDs: " . join(", ", $noAuthority) . ". ";
+			}
+		}
+		if($errors != ""){
+			if($successes == 1){
+				$errors = "Only " . $successes . " survey was successfully deleted. " . $errors;
+			}
+			else if($successes > 1){
+				$errors = "Only " . $successes . " surveys were successfully deleted. " . $errors;
+			}
+			die("false|" . $errors);
+		}
+		die("true|");
 	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
 ?>
