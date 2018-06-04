3593f311bbee133423426bb0381a5393f7f771c2
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-06-04 16:57:43 -0400

    Update batchTest.php

--- php/batchTest.php
+++ php/batchTest.php
@@ -2,13 +2,14 @@
 	require_once('orm/User.php');
 	require_once('orm/Survey.php');
 	
-	$email = $_GET["email"];
-	$salt = $_GET["salt"];
-	$selected = json_decode($_GET["selected"]);
-	$unselected = json_decode($_GET["unselected"]);
-	$filters = json_decode(rawurldecode($_GET["filters"]), true);
-	$user = User::findBySignInKey($email, $salt);
-	if(is_object($user) && get_class($user) == "User"){
+	//$email = $_GET["email"];
+	//$salt = $_GET["salt"];
+	//$selected = json_decode($_GET["selected"]);
+	//$unselected = json_decode($_GET["unselected"]);
+	//$filters = json_decode(rawurldecode($_GET["filters"]), true);
+	//$user = User::findBySignInKey($email, $salt);
+	//if(is_object($user) && get_class($user) == "User"){
+		/*
 		$successes = 0;
 		$failures = 0;
 		
@@ -17,9 +18,10 @@
 		for($i = 0; $i < count($userSites); $i++){
 			$userSiteIDs[] = $userSites[$i]->getID();
 		}
+		*/
 	
-		if($selected == "all"){
-			$surveys = Survey::findSurveysByUser($user, $filters, 0, "max");//9999999999999999999);//this might cause a timeout
+		//if($selected == "all"){
+			$surveys = Survey::findSurveysByUser(User::findByID(25), array("user"=>"aaron", "arthropod"=>"", "site"=>"", "circle"=>"", "code"=>""), 0, "max");//9999999999999999999);//this might cause a timeout
 			$selected = array();
 			for($i = 0; $i < count($surveys); $i++){
 				$surveyID = $surveys[$i]->getID() . "";
@@ -27,8 +29,9 @@
 					$selected[] = $surveyID;
 				}
 			}
-		}
-		
+		//}
+		die(json_encode($selected));
+		/*
 		$dbconn = (new Keychain)->getDatabaseConnection();
 		mysqli_query($dbconn, "DELETE ArthropodSighting FROM ArthropodSighting JOIN Survey on ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Survey.ID IN (-1, " . join(", ", $selected) . ") AND (Survey.UserFKOfObserver='" . $user->getID() . "' OR Site.ID IN (-1, " . join(", ", $userSiteIDs) . "))");
 		mysqli_query($dbconn, "DELETE Survey FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Survey.ID IN (-1, " . join(", ", $selected) . ") AND (Survey.UserFKOfObserver='" . $user->getID() . "' OR Site.ID IN (-1, " . join(", ", $userSiteIDs) . "))");
@@ -49,4 +52,5 @@
 		die("true|" . $successes);
 	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
+	*/
 ?>
