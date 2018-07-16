a51b1a721612d15d3d6d6418a375466b62419f64
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-16 15:02:09 -0400

    add survey count to data

--- php/getSites.php
+++ php/getSites.php
@@ -1,14 +1,21 @@
 <?php
   require_once("orm/Site.php");
+  require_once('orm/resources/Keychain.php');
   
   $sites = Site::findAll();
   $sitesArray = array();
   for($i = 0; $i < count($sites); $i++){
-    $sitesArray[] = array(
+    $sitesArray[strval($sites[$i]->getID())] = array(
       "ID" => $sites[$i]->getID(),
       "Name" => $sites[$i]->getName(),
       "Coordinates" => $sites[$i]->getLatitude() . "," . $sites[$i]->getLongitude(),
     );
   }
-  die(json_encode($sitesArray));
+
+  $dbconn = (new Keychain)->getDatabaseConnection();
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Plant.ID=Survey.PlantFK GROUP BY Plant.SiteFK");
+  while($row = mysqli_fetch_assoc($query)){
+    $sitesArray[strval($row["SiteFK"])]["SurveyCount"] = intval($row["SurveyCount"]);
+  }
+  die(json_encode(array_values($sitesArray)));
 ?>
