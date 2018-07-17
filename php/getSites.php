fc67a697d8b418d6bd55d61fc943506e858f6b5c
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-16 20:01:44 -0400

    add site stats

--- php/getSites.php
+++ php/getSites.php
@@ -1,21 +1,42 @@
 <?php
-  require_once("orm/Site.php");
-  require_once('orm/resources/Keychain.php');
-  
-  $sites = Site::findAll();
-  $sitesArray = array();
-  for($i = 0; $i < count($sites); $i++){
-    $sitesArray[strval($sites[$i]->getID())] = array(
-      "ID" => $sites[$i]->getID(),
-      "Name" => $sites[$i]->getName(),
-      "Coordinates" => $sites[$i]->getLatitude() . "," . $sites[$i]->getLongitude(),
-    );
-  }
+	require_once("orm/Site.php");
+	require_once('orm/resources/Keychain.php');
 
-  $dbconn = (new Keychain)->getDatabaseConnection();
+	$sites = Site::findAll();
+	$sitesArray = array();
+	for($i = 0; $i < count($sites); $i++){
+		$sitesArray[strval($sites[$i]->getID())] = array(
+			"ID" => $sites[$i]->getID(),
+			"Name" => $sites[$i]->getName(),
+			"Coordinates" => $sites[$i]->getLatitude() . "," . $sites[$i]->getLongitude(),
+			"Description" => $sites[$i]->getDescription(),
+		);
+	}
+
+	$dbconn = (new Keychain)->getDatabaseConnection();
 	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Plant.ID=Survey.PlantFK GROUP BY Plant.SiteFK");
-  while($row = mysqli_fetch_assoc($query)){
-    $sitesArray[strval($row["SiteFK"])]["SurveyCount"] = intval($row["SurveyCount"]);
-  }
-  die(json_encode(array_values($sitesArray)));
+	while($row = mysqli_fetch_assoc($query)){
+		$sitesArray[strval($row["SiteFK"])]["SurveyCount"] = intval($row["SurveyCount"]);
+	}
+
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT Survey.UserFKOfObserver) AS UserCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID GROUP BY Plant.SiteFK");
+	while($row = mysqli_fetch_assoc($query)){
+		$sitesArray[strval($row["SiteFK"])]["UserCount"] = intval($row["UserCount"]);
+	}
+
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT ArthropodSighting.Group) AS ArthropodGroupCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID JOIN ArthropodSighting ON Survey.ID=ArthropodSighting.SurveyFK GROUP BY Plant.SiteFK");
+	while($row = mysqli_fetch_assoc($query)){
+		$sitesArray[strval($row["SiteFK"])]["ArthropodGroupCount"] = intval($row["ArthropodGroupCount"]);
+	}
+
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS ArthropodCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID JOIN ArthropodSighting ON Survey.ID=ArthropodSighting.SurveyFK GROUP BY Plant.SiteFK");
+	while($row = mysqli_fetch_assoc($query)){
+		$sitesArray[strval($row["SiteFK"])]["ArthropodCount"] = intval($row["ArthropodCount"]);
+	}
+
+	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS CaterpillarCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID JOIN ArthropodSighting ON Survey.ID=ArthropodSighting.SurveyFK WHERE ArthropodSighting.Group='caterpillar' GROUP BY Plant.SiteFK");
+	while($row = mysqli_fetch_assoc($query)){
+		$sitesArray[strval($row["SiteFK"])]["CaterpillarCount"] = intval($row["CaterpillarCount"]);
+	}
+	die(json_encode(array_values($sitesArray)));
 ?>
