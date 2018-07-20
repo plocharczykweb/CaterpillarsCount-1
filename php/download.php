c92a6dd5208ec7f0081c6b85321bb50c054c0d4b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-20 14:30:59 -0400

    first draft

--- php/download.php
+++ php/download.php
@@ -9,15 +9,13 @@
     
     $dbconn = (new Keychain)->getDatabaseConnection();
     $query = mysqli_query($dbconn, "SELECT Survey.LocalDate, Survey.LocalTime, Plant.Code AS SurveyLocationCode, Plant.Circle, Plant.Orientation, Survey.PlantSpecies AS PlantSpeciesMarkedByObserver, Plant.Species AS OfficialPlantSpecies, Site.Name AS SiteName, Site.Description AS SiteDescription, Site.Latitude, Site.Longitude, Site.Region, Site.OpenToPublic AS SiteIsOpenToPublic, ArthropodSighting.Group AS ArthropodGroup, ArthropodSighting.Length AS ArthropodLength, ArthropodSighting.Quantity AS ArthropodQuantity, ArthropodSighting.PhotoURL AS ArthopodPhotoURL, ArthropodSighting.Notes AS ArthropodNotes, ArthropodSighting.Hairy AS IsCaterpillarAndIsHairy, ArthropodSighting.Rolled AS IsCaterpillarAndIsInLeafRoll, ArthropodSighting.Tented AS IsCaterpillarAndIsInSilkTent, Survey.ObservationMethod, Survey.Notes AS SurveyNotes, Survey.WetLeaves, Survey.NumberOfLeaves, Survey.AverageLeafLength, Survey.HerbivoryScore, Survey.SubmittedThroughApp AS SubmittedThroughAppInsteadOfWebsite FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID ORDER BY Survey.LocalDate DESC, Survey.LocalTime DESC");
+    mysqli_close($dbconn);
     
     //HEADERS
-    $colHeaders = array("LocalDate", "LocalTime", "SurveyLocationCode", "Circle", "Orientation", "PlantSpeciesMarkedByObserver", "OfficialPlantSpecies", "SiteName", "SiteDescription", "Latitude", "Longitude", "Region", "SiteIsOpenToPublic", "ArthropodGroup", "ArthropodLength", "ArthropodQuantity", "ArthropodPhotoURL", "ArthropodNotes", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "");
+    $colHeaders = array("LocalDate", "LocalTime", "SurveyLocationCode", "Circle", "Orientation", "PlantSpeciesMarkedByObserver", "OfficialPlantSpecies", "SiteName", "SiteDescription", "Latitude", "Longitude", "Region", "SiteIsOpenToPublic", "ArthropodGroup", "ArthropodLength", "ArthropodQuantity", "ArthropodPhotoURL", "ArthropodNotes", "IsCaterpillarAndIsHairy", "IsCaterpillarAndIsInLeafRoll", "IsCaterpillarAndIsInSilkTent", "ObservationMethod", "SurveyNotes", "WetLeaves", "NumberOfLeaves", "AverageLeafLength", "HerbivoryScore", "SubmittedThroughAppInsteadOfWebsite");
     $tableArray[] = $colHeaders;
     
     //ROWS
-    $query = mysqli_query($dbconn, "SELECT * FROM `" . $tableName . "`");
-    mysqli_close($dbconn);
-    
     while ($row = mysqli_fetch_assoc($query)){
       $rowArray = array();
       for($i = 0; $i < count($colHeaders); $i++){
@@ -28,25 +26,10 @@
     return $tableArray;
   }
 
-  function createCSV($tableName, $tableArray) {
-    if(!$fp = fopen("../iuFYr1xREQOp2ioB5MHvnCTY39UHv2/" . date("Y-m-d") . "_" . $tableName . ".csv", 'w')) return false;
-    foreach ($tableArray as $line) fputcsv($fp, $line);
-  }
-
-  function backup($tableName, $excludedHeaders=array()){
-    $tableArray = getArrayFromTable($tableName, $excludedHeaders);
-    createCSV($tableName, $tableArray);
-  }
-
-  $site = getArrayFromTable("Site", array("UserFKOfCreator", "SaltedPasswordHash", "Salt"));
-  backup("Plant");
-  backup("Survey");
-  backup("ArthropodSighting");
-  
-  SELECT * FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID
-  
-
-
-
-
+  $tableArray = getArrayFromTable();
+  $filename = "CaterpillarsCountDataAtTimestamp_" . time() . ".csv";
+  $fp = fopen($filename, 'w');
+  foreach ($tableArray as $line) fputcsv($fp, $line);
+  readfile($filename);
+  unlink($filename);
 ?>
