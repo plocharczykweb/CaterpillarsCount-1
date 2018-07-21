4ca6659175d9dd8d3666f64b4bf98d4ac97c5a67
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-21 19:30:42 -0400

    get user data

--- php/getUserRankings.php
+++ php/getUserRankings.php
@@ -1,58 +1,65 @@
 <?php
-	require_once("orm/User.php");
 	require_once('orm/resources/Keychain.php');
-	$users = User::findAll();
-	$usersArray = array();
-	for($i = 0; $i < count($users); $i++){
-		$usersArray[strval($users[$i]->getID())] = array(
-			"ID" => $users[$i]->getID(),
-			"FullName" => $users[$i]->getFullName(),
-		);
-	}
+	require_once('orm/Site.php');
+	$siteID = intval($_GET["siteID"]);
+	$siteRestriction = "<>2";
+	if($siteID > 1){$siteRestriction = "=" . $siteID;}
+	
 	$dbconn = (new Keychain)->getDatabaseConnection();
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS SurveyCount FROM Survey JOIN Plant ON Plant.ID=Survey.PlantFK GROUP BY Plant.SiteFK");
+	$query = mysqli_query($dbconn, "SELECT User.ID, CONCAT(User.FirstName, ' ', User.LastName) AS FullName, User.HiddenFromLeaderboards, SUM(CASE WHEN Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) THEN 1 ELSE 0 END) AS Week, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Month, SUM(CASE WHEN Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') THEN 1 ELSE 0 END) AS Year, Count(*) AS Total, COUNT(DISTINCT Survey.LocalDate) AS TotalUniqueDates FROM `Survey` JOIN User ON Survey.UserFKOfObserver=User.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " GROUP BY User.ID ORDER BY Year DESC");
+	
+	$rankingsArray = array();
+  	$i = 1;
 	while($row = mysqli_fetch_assoc($query)){
-		$sitesArray[strval($row["SiteFK"])]["SurveyCount"] = intval($row["SurveyCount"]);
+		$rankingsArray[strval($row["ID"])] = array(
+			"UserID" => $row["ID"],
+      			"FullName" => $row["FullName"],
+      			"HiddenFromLeaderboards" => $row["HiddenFromLeaderboards"],
+      			"Week" => intval($row["Week"]),
+			"UniqueDatesThisWeek" => 0,
+      			"Month" => intval($row["Month"]),
+			"UniqueDatesThisMonth" => 0,
+      			"Year" => intval($row["Year"]),
+			"UniqueDatesThisYear" => 0,
+      			"Total" => intval($row["Total"]),
+      			"TotalUniqueDates" => intval($row["TotalUniqueDates"]),
+    		);
 	}
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT Survey.UserFKOfObserver) AS UserCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID GROUP BY Plant.SiteFK");
+	
+	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisWeek FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) GROUP BY UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
-		$sitesArray[strval($row["SiteFK"])]["UserCount"] = intval($row["UserCount"]);
+		$rankingsArray[strval($row["UserFKOfObserver"])]["UniqueDatesThisWeek"] = intval($row["UniqueDatesThisWeek"]);
 	}
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(DISTINCT ArthropodSighting.Group) AS ArthropodGroupCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID JOIN ArthropodSighting ON Survey.ID=ArthropodSighting.SurveyFK GROUP BY Plant.SiteFK");
+	
+	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisMonth FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= STR_TO_DATE(CONCAT(DATE_FORMAT(CURDATE(),'%Y-%m'), '-01 00:00:00'), '%Y-%m-%d %T') GROUP BY UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
-		$sitesArray[strval($row["SiteFK"])]["ArthropodGroupCount"] = intval($row["ArthropodGroupCount"]);
+		$rankingsArray[strval($row["UserFKOfObserver"])]["UniqueDatesThisMonth"] = intval($row["UniqueDatesThisMonth"]);
 	}
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS ArthropodCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID JOIN ArthropodSighting ON Survey.ID=ArthropodSighting.SurveyFK GROUP BY Plant.SiteFK");
+
+	$query = mysqli_query($dbconn, "SELECT UserFKOfObserver, COUNT(DISTINCT LocalDate) AS UniqueDatesThisYear FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK" . $siteRestriction . " AND Survey.LocalDate >= STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-01-01 00:00:00'), '%Y-%m-%d %T') GROUP BY UserFKOfObserver");
 	while($row = mysqli_fetch_assoc($query)){
-		$sitesArray[strval($row["SiteFK"])]["ArthropodCount"] = intval($row["ArthropodCount"]);
+		$rankingsArray[strval($row["UserFKOfObserver"])]["UniqueDatesThisYear"] = intval($row["UniqueDatesThisYear"]);
 	}
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, COUNT(*) AS CaterpillarCount FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID JOIN ArthropodSighting ON Survey.ID=ArthropodSighting.SurveyFK WHERE ArthropodSighting.Group='caterpillar' GROUP BY Plant.SiteFK");
-	while($row = mysqli_fetch_assoc($query)){
-		$sitesArray[strval($row["SiteFK"])]["CaterpillarCount"] = intval($row["CaterpillarCount"]);
-	}
-	$query = mysqli_query($dbconn, "SELECT Plant.SiteFK, MAX(STR_TO_DATE(CONCAT(Survey.LocalDate, ' ', Survey.LocalTime), '%Y-%m-%d %T')) AS MostRecentDateTime FROM `Survey` JOIN Plant ON Survey.PlantFK=Plant.ID GROUP BY Plant.SiteFK");
-	while($row = mysqli_fetch_assoc($query)){
-		$sitesArray[strval($row["SiteFK"])]["MostRecentDateTime"] = $row["MostRecentDateTime"];
-	}
-	for($i = 0; $i < count($sites); $i++){
-		if(!array_key_exists("SurveyCount", $sitesArray[strval($sites[$i]->getID())])){
-			$sitesArray[strval($sites[$i]->getID())]["SurveyCount"] = 0;
-		}
-		if(!array_key_exists("UserCount", $sitesArray[strval($sites[$i]->getID())])){
-			$sitesArray[strval($sites[$i]->getID())]["UserCount"] = 0;
-		}
-		if(!array_key_exists("ArthropodGroupCount", $sitesArray[strval($sites[$i]->getID())])){
-			$sitesArray[strval($sites[$i]->getID())]["ArthropodGroupCount"] = 0;
-		}
-		if(!array_key_exists("ArthropodCount", $sitesArray[strval($sites[$i]->getID())])){
-			$sitesArray[strval($sites[$i]->getID())]["ArthropodCount"] = 0;
-		}
-		if(!array_key_exists("CaterpillarCount", $sitesArray[strval($sites[$i]->getID())])){
-			$sitesArray[strval($sites[$i]->getID())]["CaterpillarCount"] = 0;
-		}
-		if(!array_key_exists("MostRecentDateTime", $sitesArray[strval($sites[$i]->getID())])){
-			$sitesArray[strval($sites[$i]->getID())]["MostRecentDateTime"] = "Never";
+	mysqli_close($dbconn);
+	
+	$allUsers = User::findAll();
+	for($j = 0; $j < count($allUsers); $j++){
+		if(is_object($allUsers[$j]) && get_class($allUsers[$j]) == "User" && !array_key_exists(strval($allUsers[$j]->getID()), $rankingsArray)){
+			$rankingsArray[strval($allUsers[$j]->getID())] = array(
+				"UserID" => $allUsers[$j]->getID(),
+				"Name" => $allUsers[$j]->getFullName(),
+				"Week" => 0,
+				"UniqueDatesThisWeek" => 0,
+				"Month" => 0,
+				"UniqueDatesThisMonth" => 0,
+				"Year" => 0,
+				"UniqueDatesThisYear" => 0,
+				"Total" => 0,
+      				"TotalUniqueDates" => 0,
+			);
 		}
 	}
-	die(json_encode(array_values($sitesArray)));
+
+	die(json_encode(array_values($rankingsArray)));
+	
 ?>
