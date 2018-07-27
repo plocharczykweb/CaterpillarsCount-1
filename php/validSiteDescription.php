2c0b26dd81afd12084c619d11d394e8013ce4697
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-27 00:26:09 -0400

    Create validSiteDescription.php

---
+++ php/validSiteDescription.php
@@ -0,0 +1,8 @@
+<?php
+  require_once("orm/resources/Keychain.php");
+  require_once("orm/Site.php");
+  $dbconn = (new Keychain)->getDatabaseConnection();
+  
+  $description="hello https://google.com, how are you http://yahoo.com https://bing.com https://ask.com?";
+  $description = Site::validDescription($dbconn, $description);
+?>
