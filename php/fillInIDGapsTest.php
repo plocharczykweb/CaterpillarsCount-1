82608ec1e64ceda54dc096983bf5593cc383aa01
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-26 22:13:54 -0400

    add line breaks

--- php/fillInIDGapsTest.php
+++ php/fillInIDGapsTest.php
@@ -1,9 +1,10 @@
 <?php
 require_once('orm/resources/Keychain.php');
 $dbconn = (new Keychain)->getDatabaseConnection();
+$id = 1261;
   $query = mysqli_query($dbconn, "SELECT t1.ID+1 AS NextID FROM `Plant` AS t1 LEFT JOIN `Plant` AS t2 ON t1.ID+1=t2.ID WHERE t2.ID IS NULL");
 			while(($row = mysqli_fetch_assoc($query) && $id = intval($row["NextID"])) || $id += 1){
-				echo $id;
+				echo $id . "<br/>";
         if($id > 3700){break;}
         //if(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `Plant` WHERE `Code`='" . self::IDToCode($id) . "' LIMIT 1")) == 0){
 					//break;
