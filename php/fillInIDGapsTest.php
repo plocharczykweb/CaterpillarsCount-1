ba2bcf1eec8a458272daa5f2d671d8c4f7c35a92
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-26 22:31:32 -0400

    Update fillInIDGapsTest.php

--- php/fillInIDGapsTest.php
+++ php/fillInIDGapsTest.php
@@ -1,7 +1,9 @@
 <?php
 	require_once('orm/resources/Keychain.php');
+	require_once('orm/Plant.php');
 	$dbconn = (new Keychain)->getDatabaseConnection();
 	$id = 1261;
+	echo Plant::IDToCode($id) . "<br/>";
 	$query = mysqli_query($dbconn, "SELECT t1.ID+1 AS NextID FROM `Plant` AS t1 LEFT JOIN `Plant` AS t2 ON t1.ID+1=t2.ID WHERE t2.ID IS NULL");
 	while($row = mysqli_fetch_assoc($query)){
 		$id = intval($row["NextID"]);
