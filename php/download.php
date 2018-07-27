9fde1c9d914530e2e6149f2218d6c3a810c3546b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-27 18:24:29 -0400

    close dbconn after work is done

--- php/download.php
+++ php/download.php
@@ -8,7 +8,6 @@
     
     $dbconn = (new Keychain)->getDatabaseConnection();
     $query = mysqli_query($dbconn, "SELECT Survey.ID, Survey.LocalDate, SUBSTRING(Survey.LocalTime, 0, 5) AS LocalTime, Plant.Code AS SurveyLocationCode, Plant.Circle, Plant.Orientation, Survey.PlantSpecies AS PlantSpeciesMarkedByObserver, Plant.Species AS OfficialPlantSpecies, Site.Name AS SiteName, Site.Description AS SiteDescription, Site.Latitude, Site.Longitude, Site.Region, ArthropodSighting.Group AS ArthropodGroup, ArthropodSighting.Length AS ArthropodLength, ArthropodSighting.Quantity AS ArthropodQuantity, IF(ArthropodSighting.PhotoURL='','',CONCAT('https://caterpillarscount.unc.edu/images/arthropods/', ArthropodSighting.PhotoURL)) AS ArthropodPhotoURL, ArthropodSighting.Notes AS ArthropodNotes, ArthropodSighting.Hairy AS IsCaterpillarAndIsHairy, ArthropodSighting.Rolled AS IsCaterpillarAndIsInLeafRoll, ArthropodSighting.Tented AS IsCaterpillarAndIsInSilkTent, Survey.ObservationMethod, Survey.Notes AS SurveyNotes, Survey.WetLeaves, Survey.NumberOfLeaves, Survey.AverageLeafLength, Survey.HerbivoryScore FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID ORDER BY Survey.LocalDate DESC, Survey.LocalTime DESC");
-    mysqli_close($dbconn);
     
     //HEADERS
     $colHeaders = array("SiteName", 
@@ -51,6 +50,7 @@
     }
     
     $query = mysqli_query($dbconn, "SELECT Survey.LocalDate, SUBSTRING(Survey.LocalTime, 0, 5) AS LocalTime, Plant.Code AS SurveyLocationCode, Plant.Circle, Plant.Orientation, Survey.PlantSpecies AS PlantSpeciesMarkedByObserver, Plant.Species AS OfficialPlantSpecies, Site.Name AS SiteName, Site.Description AS SiteDescription, Site.Latitude, Site.Longitude, Site.Region, Survey.ObservationMethod, Survey.Notes AS SurveyNotes, Survey.WetLeaves, Survey.NumberOfLeaves, Survey.AverageLeafLength, Survey.HerbivoryScore FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID JOIN Site ON Plant.SiteFK=Site.ID WHERE Survey.ID NOT IN (" . join(", ", $surveyIDsWithSightings) . ")");
+    mysqli_close($dbconn);
     while ($row = mysqli_fetch_assoc($query)){
       $rowArray = array();
       for($i = 0; $i < count($colHeaders); $i++){
