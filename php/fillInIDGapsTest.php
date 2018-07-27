951ec77e98639512e7da7accaf86a5eb990651d3
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-26 23:22:10 -0400

    rolling fill in

--- php/fillInIDGapsTest.php
+++ php/fillInIDGapsTest.php
@@ -7,6 +7,12 @@
 	$query = mysqli_query($dbconn, "SELECT t1.ID+1 AS NextID FROM `Plant` AS t1 LEFT JOIN `Plant` AS t2 ON t1.ID+1=t2.ID WHERE t2.ID IS NULL");
 	while($row = mysqli_fetch_assoc($query)){
 		$id = intval($row["NextID"]);
-		echo $id . "<br/>";
+		while(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `Plant` WHERE `ID`='" . $id . "' LIMIT 1")) == 0){
+			echo $id;
+			if($id > 3700){
+				break 2;
+			}
+			$id++;
+		}
 	}
 ?>
