5ed43213b9b218564c6c9142c79579c8b601a7b0
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-27 20:17:20 -0400

    Create getHiddenFromLeaderboards.php

---
+++ php/getHiddenFromLeaderboards.php
@@ -0,0 +1,13 @@
+<?php
+	require_once('orm/resources/Keychain.php');
+	require_once('User.php');
+  
+	$email = $_GET["email"];
+	$salt = $_GET["salt"];
+  
+  	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User"){
+    		die("true|" . ($user->getHiddenFromLeaderboards() ? 'true' : 'false'));
+  	}
+	die("false|Your log in dissolved. Maybe you logged in on another device.");
+?>
