a100a771b1995949614fe44ccf21e1c7928c2b0c
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-27 20:54:25 -0400

    return bools

--- php/getPrivacySettings.php
+++ php/getPrivacySettings.php
@@ -8,7 +8,7 @@
   	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
 		$privacySettings = array(
-			"HiddenFromLeaderboards" => ($user->getHiddenFromLeaderboards() ? 'true' : 'false')
+			"HiddenFromLeaderboards" => $user->getHiddenFromLeaderboards()
 		);
     		die("true|" . json_encode($privacySettings));
   	}
