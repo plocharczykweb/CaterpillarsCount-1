9f2a856c5ea80ee6d41350000a0814fd7c16b899
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-07-27 21:23:39 -0400

    set instead of toggle

--- php/setHiddenFromLeaderboards.php
+++ php/setHiddenFromLeaderboards.php
@@ -4,10 +4,11 @@
   
 	$email = $_GET["email"];
 	$salt = $_GET["salt"];
+	$newValue = filter_var($_GET["newValue"], FILTER_VALIDATE_BOOLEAN);
   
   	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User"){
-    		$user->setHiddenFromLeaderboards(!$user->getHiddenFromLeaderboards());
+    		$user->setHiddenFromLeaderboards($newValue);
     		die("true|" . ($user->getHiddenFromLeaderboards() ? 'true' : 'false'));
   	}
 	die("false|Your log in dissolved. Maybe you logged in on another device.");
