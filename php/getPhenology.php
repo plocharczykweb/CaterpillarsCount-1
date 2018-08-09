ca4e1b7a1350d269a8f9681d1e28654abcf2782d
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-09 01:44:55 -0400

    label each line

--- php/getPhenology.php
+++ php/getPhenology.php
@@ -1,16 +1,36 @@
 <?php
 	require_once('orm/resources/Keychain.php');
+	require_once('orm/Site.php');
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
-  
+	
   	$lines = json_decode($_GET["lines"], true);
+	$readableArthropods = array(
+		"%" => "all arthropods",
+		"ant" => "ants",
+		"aphid" => "aphids and psyllids",
+		"bee" => "bees and wasps",
+		"beetle" => "beetles",
+		"caterpillar" => "caterpillars",
+		"daddylonglegs" => "daddy longlegs",
+		"fly" => "flies",
+		"grasshopper" => "grasshoppers and crickets",
+		"leafhopper" => "leaf hoppers and cicadas",
+		"moths" => "butterflies and moths",
+		"spider" => "spiders",
+		"truebugs" => "true bugs",
+		"other" => "\"other\" arthropods",
+		"unidentified" => "unidentified arthropods"
+	);
   
   	$weightedLines = array();
   	for($i = 0; $i < count($lines); $i++){
+		$siteID = intval($lines[$i]["siteID"]);
+		$site = Site::findByID(siteID);
+		if(!is_object($site) || get_class($site) != "Site"){continue;}
     		$densityOrOccurrence = $lines[$i]["densityOrOccurrence"];
-		$siteID = $lines[$i]["siteID"];
-		$arthropod = $lines[$i]["arthropod"];
-		$year = $lines[$i]["year"];
+		$arthropod = mysqli_real_escape_string($dbconn, $lines[$i]["arthropod"]);
+		$year = intval($lines[$i]["year"]);
     
     		$dateWeights = array();
     
@@ -41,8 +61,8 @@
 			$dateWeights[$j] = array($dateWeights[$j][0], (($dateWeights[$j][1] / $dateWeights[$j][2]) * 100));
 		}
     
-    		$weightedLines[] = $dateWeights;
+    		$weightedLines[ucfirst($densityOrOccurrence) . " of " . $readableArthropods[$arthropod] . " at " . $site->getName() . " in " . $year] = $dateWeights;
   	}
   	mysqli_close($dbconn);
-  	die(json_encode($weightedLines));//in the form of: [[[LOCAL_DATE, PERCENT]]] //example: [[[2018-08-09, 30], [2018-08-12, 25]], [[2018-08-15, 21.3], [2018-09-02, 70]]]
+  	die(json_encode($weightedLines));//in the form of: [LABEL: [[LOCAL_DATE, PERCENT]]] //example: ["Density of all arthropods at Example Site in 2018": [[2018-08-09, 30], [2018-08-12, 25]], [[2018-08-15, 21.3], [2018-09-02, 70]]]
 ?>
