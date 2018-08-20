f4e700578c8d0bd1d7caefd9630e7c820f13d73f
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-20 00:39:42 -0400

    try not sorting

--- php/getComposition.php
+++ php/getComposition.php
@@ -198,10 +198,10 @@
 		$query = mysqli_query($dbconn, "SELECT Plant.Species, SUM(ArthropodSighting.Quantity) AS ArthropodCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='$siteID' GROUP BY Plant.Species");
 		while($row = mysqli_fetch_assoc($query)){
 			if($totalDensity[$row["Species"]] > 0){
-				$totalDensity[$row["Species"]] = floatval($row["ArthropodCount"]) / floatval($totalDensity[$row["Species"]]);
+				$totalDensity[$row["Species"]] = floatval($row["ArthropodCount"]) / $totalDensity[$row["Species"]];
 			}
 		}
-		sort($totalDensity);
+		//sort($totalDensity);
 		
  		if($comparisonMetric == "occurrence"){
  			$arthropodOccurrencesSet = array();
