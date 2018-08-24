ccc1591d219d128ff20af7ea1abe876411b80eef
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-23 20:33:29 -0400

    Update forceCreateNewUser.php

--- php/forceCreateNewUser.php
+++ php/forceCreateNewUser.php
@@ -6,9 +6,9 @@
   $firstName = $_GET["first"];
   $lastName = $_GET["last"];
   $email = $_GET["email"];
-  $salt = "";
-  $saltedPasswordHash = "";
-  
+  $salt = mysqli_real_escape_string($dbconn, hash("sha512", rand() . rand() . rand()));
+  $saltedPasswordHash = mysqli_real_escape_string($dbconn, hash("sha512", $salt . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9)));
+
   mysqli_query($dbconn, "INSERT INTO User (`FirstName`, `LastName`, `DesiredEmail`, `Email`, `Salt`, `SaltedPasswordHash`) VALUES ('$firstName', '$lastName', '$email', '$email', '$salt', '$saltedPasswordHash')");
   $id = intval(mysqli_insert_id($dbconn));
   mysqli_close($dbconn);
