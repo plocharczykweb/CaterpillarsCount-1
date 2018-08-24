17a9ee322628bd4b49de918673213cb3b0686af1
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-23 21:53:22 -0400

    Update forceCreateNewUser.php

--- php/forceCreateNewUser.php
+++ php/forceCreateNewUser.php
@@ -6,11 +6,15 @@
   $firstName = $_GET["first"];
   $lastName = $_GET["last"];
   $email = $_GET["email"];
-  $salt = mysqli_real_escape_string($dbconn, hash("sha512", rand() . rand() . rand()));
-  $saltedPasswordHash = mysqli_real_escape_string($dbconn, hash("sha512", $salt . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9)));
 
-  mysqli_query($dbconn, "INSERT INTO User (`FirstName`, `LastName`, `DesiredEmail`, `Email`, `Salt`, `SaltedPasswordHash`) VALUES ('$firstName', '$lastName', '$email', '$email', '$salt', '$saltedPasswordHash')");
-  $id = intval(mysqli_insert_id($dbconn));
-  mysqli_close($dbconn);
-  die($id . "");
+  if(mysqli_num_rows(mysqli_query($dbconn, "SELECT `ID` FROM `User` WHERE `Email`='" . $email . "' LIMIT 1")) == 0){
+    $salt = mysqli_real_escape_string($dbconn, hash("sha512", rand() . rand() . rand()));
+    $saltedPasswordHash = mysqli_real_escape_string($dbconn, hash("sha512", $salt . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9)));
+
+    mysqli_query($dbconn, "INSERT INTO User (`FirstName`, `LastName`, `DesiredEmail`, `Email`, `Salt`, `SaltedPasswordHash`) VALUES ('$firstName', '$lastName', '$email', '$email', '$salt', '$saltedPasswordHash')");
+    $id = intval(mysqli_insert_id($dbconn));
+    mysqli_close($dbconn);
+    die($id . "");
+  }
+  die($email);
 ?>
