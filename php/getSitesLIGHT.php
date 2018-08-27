c7dbab5f660623425d32c0ca95d2b0eacdd3634b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-26 20:00:19 -0400

    Create getSitesLIGHT.php

---
+++ php/getSitesLIGHT.php
@@ -0,0 +1,13 @@
+<?php
+	require_once("orm/Site.php");
+  
+	$sites = Site::findAll();
+	$sitesArray = array();
+	for($i = 0; $i < count($sites); $i++){
+		$sitesArray[] = array(
+			"ID" => $sites[$i]->getID(),
+			"Name" => $sites[$i]->getName(),
+		);
+	}
+	die(json_encode($sitesArray));
+?>
