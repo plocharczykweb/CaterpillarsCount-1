35afa96d042066b676869b3ec3b9da435c053c80
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-27 14:50:57 -0400

    Create getYearRange.php

---
+++ php/getYearRange.php
@@ -0,0 +1,9 @@
+<?php
+	require_once('orm/resources/Keychain.php');
+	$siteID = intval($_GET["siteID"]);
+	$dbconn = (new Keychain)->getDatabaseConnection();
+  
+	$query = mysqli_query($dbconn, "SELECT MIN(YEAR(LocalDate)) AS EarliestYear, MAX(YEAR(LocalDate)) AS LatestYear FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK LIKE '$siteID'");
+	$row = mysqli_fetch_assoc($query);
+  die(json_encode(array($row["EarliestYear"], $row["LatestYear"])));
+?>
