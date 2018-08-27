7388d21d3cdcec7751ab0acd2ec644b04a22469d
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-08-27 14:24:27 -0400

    allow $siteID parameter

--- php/getEarliestYear.php
+++ php/getEarliestYear.php
@@ -1,8 +1,14 @@
 <?php
 	require_once('orm/resources/Keychain.php');
+
+	$siteID = $_GET["siteID"];
+	$extraSQL = "";
+	if(isset($siteID)){
+		$extraSQL = " JOIN Plant ON Survey.PlantFK=Plant.ID WHERE Plant.SiteFK='" . intval($siteID) . "'";
+	}
 	
 	$dbconn = (new Keychain)->getDatabaseConnection();
   
-	$query = mysqli_query($dbconn, "SELECT YEAR(LocalDate) AS EarliestYear FROM Survey ORDER BY LocalDate ASC LIMIT 1");
+	$query = mysqli_query($dbconn, "SELECT YEAR(LocalDate) AS EarliestYear FROM Survey" . $extraSQL . " ORDER BY LocalDate ASC LIMIT 1");
 	die(mysqli_fetch_assoc($query)["EarliestYear"]);
 ?>
