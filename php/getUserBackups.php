60ab383eeca9f44b75546d8edab69f8490f086e8
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-07 19:12:46 -0400

    dont compare encoded string to string

--- php/getUserBackups.php
+++ php/getUserBackups.php
@@ -1,18 +1,17 @@
 <?php
-  require_once('orm/User.php');
-  
-  $email = $_GET["email"];
+	require_once('orm/User.php');
+	$email = $_GET["email"];
 	$salt = $_GET["salt"];
   
-  $user = User::findBySignInKey($email, $salt);
-	if(($email == "plocharczykweb@gmail.com" || $email == "hurlbert@bio.unc.edu") && is_object($user) && get_class($user) == "User"){
-    $files = scandir("../yds3jk92345bfjHU874eD");
-    for($i = 0; $i < count($files); $i++){
-      if(strpos($files[$i], ".csv") === false){
-        unset($files[$i]);
-      }
-    }
-    die("true|" . json_encode($files));
-  }
-  die("false|");
+  	$user = User::findBySignInKey($email, $salt);
+	if(is_object($user) && get_class($user) == "User" && ($user->getEmail() == "plocharczykweb@gmail.com" || $user->getEmail() == "hurlbert@bio.unc.edu")){
+    		$files = scandir("../yds3jk92345bfjHU874eD");
+    		for($i = 0; $i < count($files); $i++){
+      		if(strpos($files[$i], ".csv") === false){
+        		unset($files[$i]);
+      		}
+    		}
+    		die("true|" . json_encode($files));
+  	}
+  	die("false|");
 ?>
