16da56d9f46c0d85e3839d73a4268dbadab40431
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-07 22:57:59 -0400

    Delete movePublicBackups.php

--- php/movePublicBackups.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $files = scandir("../" . getenv("USER_BACKUPS"));
-  for($i = 0; $i < count($files); $i++){
-    if(strpos($files[$i], "Site") !== false){
-      rename("../" . getenv("USER_BACKUPS") . "/" . $files[$i], "../backups/" . $files[$i]);
-    }
-  }
-?>
