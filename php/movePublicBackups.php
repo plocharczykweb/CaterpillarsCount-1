bba99e1f4d867ab327f8e165d9bd2e720d83d52b
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-07 22:48:18 -0400

    move site stuff

--- php/movePublicBackups.php
+++ php/movePublicBackups.php
@@ -1,7 +1,7 @@
 <?php
   $files = scandir("../" . getenv("USER_BACKUPS"));
   for($i = 0; $i < count($files); $i++){
-    if(strpos($files[$i], "User") === false){
+    if(strpos($files[$i], "Site") !== false){
       rename("../" . getenv("USER_BACKUPS") . "/" . $files[$i], "../backups/" . $files[$i]);
     }
   }
