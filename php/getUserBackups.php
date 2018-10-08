d075f262c0d8fee395e3059ec73be9998e2e1ac4
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-07 21:22:00 -0400

    reverse for loop

--- php/getUserBackups.php
+++ php/getUserBackups.php
@@ -6,7 +6,7 @@
   	$user = User::findBySignInKey($email, $salt);
 	if(is_object($user) && get_class($user) == "User" && ($user->getEmail() == "plocharczykweb@gmail.com" || $user->getEmail() == "hurlbert@bio.unc.edu")){
     		$files = array_values(scandir("../yds3jk92345bfjHU874eD"));
-    		for($i = 0; $i < count($files); $i++){
+    		for($i = (count($files) - 1); $i >= 0; $i--){
 			if(strpos($files[$i], ".csv") === false){
 				unset($files[$i]);
 			}
