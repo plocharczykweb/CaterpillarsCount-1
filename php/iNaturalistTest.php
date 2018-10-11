4c9f1913debc4eb4a26bd77b9bfbbc1ee0f3f938
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-10 20:55:20 -0400

    get token

--- php/iNaturalistTest.php
+++ php/iNaturalistTest.php
@@ -1,13 +1,11 @@
 <?php
-  $url = 'https://www.inaturalist.org/oauth/token';
-  $params = "client_id=" . getenv("iNaturalistAppID") . "&client_secret=" . getenv("iNaturalistAppSecret") . "&grant_type=password&username=caterpillarscountdev&password=" . getenv("iNaturalistPassword");
-  $ch = curl_init( $url );
-  curl_setopt( $ch, CURLOPT_POST, 1);
-  curl_setopt( $ch, CURLOPT_POSTFIELDS, $params);
-  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt( $ch, CURLOPT_HEADER, 0);
-  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+  $ch = curl_init('https://www.inaturalist.org/oauth/token');
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=" . getenv("iNaturalistAppID") . "&client_secret=" . getenv("iNaturalistAppSecret") . "&grant_type=password&username=caterpillarscountdev&password=" . getenv("iNaturalistPassword"));
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-  $response = curl_exec( $ch );
-  die($response);
+  $token = curl_exec($ch)["access_token"];
+  die($token);
 ?>
