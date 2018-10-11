8e4e1d67134db942d7c10905fb623f8ec0cc9423
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-11 17:48:38 -0400

    die observation id

--- php/iNaturalistTest.php
+++ php/iNaturalistTest.php
@@ -1,5 +1,9 @@
 <?php
 	require_once("orm/Plant.php");
+
+	function cleanParam($param){
+		return preg_replace('!\s+!', '-', trim(preg_replace('/[^a-zA-Z0-9.]+/', ' ', $param)));
+	}
 	
 	$ch = curl_init('https://www.inaturalist.org/oauth/token');
 	curl_setopt($ch, CURLOPT_POST, 1);
@@ -21,14 +25,14 @@ $arthropodNotes = "test run";
 $arthropodLength = "21";
 $arthropodQuantity = "5";
 $userTag = "ccdev";
-	$url = "http://www.inaturalist.org/observations.json?observation[species_guess]=" . $order . "&observation[id_please]=1&observation[observed_on_string]=" . $date . "&observation[place_guess]=" . $site->getName() . "&observation[latitude]=" . $site->getLatitude() . "&observation[longitude]=" . $site->getLongitude();
+	$url = "http://www.inaturalist.org/observations.json?observation[species_guess]=" . cleanParam($order) . "&observation[id_please]=1&observation[observed_on_string]=" . cleanParam($date) . "&observation[place_guess]=" . cleanParam($site->getName()) . "&observation[latitude]=" . cleanParam($site->getLatitude()) . "&observation[longitude]=" . cleanParam($site->getLongitude());
 	if($arthropodNotes != ""){
 		$url .= "&observation[description]=" . $arthropodNotes;
 	}
 	$params = [["1289", $arthropodLength], ["1194", $site->getName()], ["5715", $plant->getCircle()], ["1422", $plantCode], ["306", $plant->getSpecies()], ["5712", $numberOfLeaves], ["5711", $herbivoryScore], ["5748", $arthropodQuantity], ["5710", $userTag]];
         $observationFieldIDString = "&observation[observation_field_values_attributes]";
 	for($i = 0; $i < count($params); $i++){
-		$url .= $observationFieldIDString . "[" . $i . "][observation_field_id]=" . $params[$i][0] . $observationFieldIDString . "[" . $i . "][value]=" . $params[$i][1];
+		$url .= $observationFieldIDString . "[" . $i . "][observation_field_id]=" . cleanParam($params[$i][0]) . $observationFieldIDString . "[" . $i . "][value]=" . cleanParam($params[$i][1]);
 	}
 	if(strpos($order, "caterpillar") !== false){
 		$url .= $observationFieldIDString . "[10][observation_field_id]=3441" . $observationFieldIDString . "[10][value]=caterpillar";
@@ -41,14 +45,14 @@ $userTag = "ccdev";
 
 	echo $url;
 
-	$ch = curl_init(preg_replace('!\s+!', '-', $url));//urlencode(
+	$ch = curl_init($url);//urlencode(
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, "access_token=" . $token);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-	$result = curl_exec($ch);
+	$observation = json_decode(curl_exec($ch), true)[0];
 
-	echo $result;
+	echo $observation["id"];
 ?>
