01ef848a72b365b63f76483e9989a222296d1084
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-12 01:17:54 -0400

    correct photo params

--- php/submitToINaturalist.php
+++ php/submitToINaturalist.php
@@ -122,7 +122,7 @@
 		//LINK OBSERVATION TO CATERPILLARS COUNT PROJECT
 		$ch = curl_init("http://www.inaturalist.org/project_observations");
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "access_token=" . $token . "&project_observation[observation_id]=" . $observation["id"] . "&project_observation[project_id]=5443");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "access_token"=> $token, "observation_photo[observation_id]"=> $observation["id"]);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
