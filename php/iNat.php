ee48934ceac3aa47f221cbdbb2b9c07745f34bc9
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-18 17:37:20 -0400

    custom myUrlEncode

--- php/iNat.php
+++ php/iNat.php
@@ -1,12 +1,16 @@
 <?php
 	require_once("orm/Plant.php");
-	function myUrlEncode($string) {
-	    $entities = array('%20');
-	    $replacements = array(' ');
-	    return str_replace($entities, $replacements, $string);
+	function r($search, $replace, $subject){
+		return str_replace($search, $replace, $subject);
 	}
+	function myUrlEncode($string) {
+	    return r(" ", "%20", r("!", "%21", r("*", "%2A", r("'", "%27", r("(", "%28", r(")", "%29", r(";", "%3B", r(":", "%3A", r("@", "%40", r("&", "%26", r("=", "%3D", r("+", "%2B", r("$", "%24", r(",", "%2C", r("/", "%2F", r("?", "%3F", r("%", "%25", r("#", "%23", r("[", "%5B", r("]", "%5D", $string))))))))))))))))))));
+	}
+
+array();
+    $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
 	function cleanParam($param){
-		$param = preg_replace('!\s+!', '%20', trim(preg_replace('/[^a-zA-Z0-9.-]/', ' ', trim((string)$param))));
+		$param = myUrlEncode(preg_replace('!\s+!', ' ', trim(preg_replace('/[^a-zA-Z0-9.-]/', ' ', trim((string)$param)))));
 		if($param == ""){
 			return "None";
 		}
