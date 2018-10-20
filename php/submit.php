2ddf8f1dd872ddd285ab5cd48f35434458122221
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-20 17:53:12 -0400

    pass iNaturalistObserverID

--- php/submit.php
+++ php/submit.php
@@ -92,7 +92,7 @@
 							$arthropodSightingFailures .= strval($attachResult);
 						}
 						if($arthropodSighting->getPhotoURL() != "" && $user->getEmail() == "plocharczykweb@gmail.com"){// && $site->getName() != "Example Site"){
-							submitINaturalistObservation("ccdev", $plant->getCode(), $survey->getLocalDate(), $survey->getObservationMethod(), $survey->getNotes(), $survey->getWetLeaves(), $arthropodSighting->getGroup(), $arthropodSighting->getHairy(), $arthropodSighting->getRolled(), $arthropodSighting->getTented(), $arthropodSighting->getQuantity(), $arthropodSighting->getLength(), $arthropodSighting->getPhotoURL(), $arthropodSighting->getNotes(), $survey->getNumberOfLeaves(), $survey->getAverageLeafLength(), $survey->getHerbivoryScore());
+							submitINaturalistObservation($user->getINaturalistObserverID(), $plant->getCode(), $survey->getLocalDate(), $survey->getObservationMethod(), $survey->getNotes(), $survey->getWetLeaves(), $arthropodSighting->getGroup(), $arthropodSighting->getHairy(), $arthropodSighting->getRolled(), $arthropodSighting->getTented(), $arthropodSighting->getQuantity(), $arthropodSighting->getLength(), $arthropodSighting->getPhotoURL(), $arthropodSighting->getNotes(), $survey->getNumberOfLeaves(), $survey->getAverageLeafLength(), $survey->getHerbivoryScore());
 						}
 					}
 					else{
