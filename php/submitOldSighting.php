51bcd102d111e940753fea5c4f0dc136d9f08af4
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-10-21 00:59:59 -0400

    Create submitOldSighting.php

---
+++ php/submitOldSighting.php
@@ -0,0 +1,13 @@
+<?php
+  require_once("orm/ArthropodSighting.php");
+  require_once("submitToINaturalist.php");
+  
+  $arthropodSightingID = $_GET["id"];
+  
+  $arthropodSighting = ArthropodSighting::findByID($arthropodSightingID);
+  $survey = $arthropodSighting->getSurvey();
+  $plant = $survey->getPlant();
+  $site = $plant->getSite();
+  
+  submitINaturalistObservation($user->getINaturalistObserverID(), $plant->getCode(), $survey->getLocalDate(), $survey->getObservationMethod(), $survey->getNotes(), $survey->getWetLeaves(), $arthropodSighting->getGroup(), $arthropodSighting->getHairy(), $arthropodSighting->getRolled(), $arthropodSighting->getTented(), $arthropodSighting->getQuantity(), $arthropodSighting->getLength(), $arthropodSighting->getPhotoURL(), $arthropodSighting->getNotes(), $survey->getNumberOfLeaves(), $survey->getAverageLeafLength(), $survey->getHerbivoryScore());
+?>
