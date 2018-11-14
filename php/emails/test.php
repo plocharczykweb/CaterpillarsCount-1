18f8531e7121e7d45539acc2a8fd218f0cc6011d
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-14 06:21:22 -0500

    add first name

--- php/emails/test.php
+++ php/emails/test.php
@@ -3,6 +3,6 @@
   
   require_once("../orm/resources/mailing.php");
   
-  email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", $firstName);
-  email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", $firstName);
+  email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Aaron");
+  email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Aaron");
 ?>
