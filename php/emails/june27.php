bbf0d9ab1c656ec9896d282990e9ccee3769d3a0
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-14 18:55:01 -0500

    add conditions and email6

--- php/emails/june27.php
+++ php/emails/june27.php
@@ -6,17 +6,22 @@
   require_once("../orm/resources/mailing.php");
   
   $sites = Site::findAll();
+  $dbconn = (new Keychain)->getDatabaseConnection();
   for($i = 0; $i < count($sites); $i++){
     if($sites[$i]->getActive() && $sites[$i]->getNumberOfSurveysByYear(date("Y")) <= 2){
-      $emails = $sites[$i]->getAuthorityEmails();
-      for($j = 0; $j < count($emails); $j++){
-        $firstName = "there";
-        $user = User::findByEmail();
-        if(is_object($user) && get_class($user) != "User"){
-          $firstName = $user->getFirstName();
+      $query = mysqli_query($dbconn, "SELECT COUNT(Survey.*) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $$sites[$i]->getID() . "' AND Survey.LocalDate>'" . date("Y") . "-06-13'");
+		  if(intval(mysqli_fetch_assoc($query)["Count"]) == 0){
+        $emails = $sites[$i]->getAuthorityEmails();
+        for($j = 0; $j < count($emails); $j++){
+          $firstName = "there";
+          $user = User::findByEmail();
+          if(is_object($user) && get_class($user) != "User"){
+            $firstName = $user->getFirstName();
+          }
+          email6($emails[$j], "Having Survey Issues?", $sites[$i]->getName());
         }
-        email4($emails[$j], "The Caterpillars Count! Season Has Begun!", $firstName);
       }
     }
   }
+  mysqli_close($dbconn);
 ?>
