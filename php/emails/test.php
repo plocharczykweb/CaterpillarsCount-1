e5cf9293313447e3b9bf429fe767e9f828856ed6
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-15 03:33:11 -0500

    final test

--- php/emails/test.php
+++ php/emails/test.php
@@ -4,9 +4,9 @@
   require_once("../orm/Site.php");
   require_once("../orm/resources/mailing.php");
   
-  email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Aaron");
-  email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Aaron");
+  email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Allen");
+  email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
   email6("plocharczykweb@gmail.com", "Touching Base about Example Site", "Example Site");
   email7("plocharczykweb@gmail.com", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
-  email8("plocharczykweb@gmail.com", "Check Your Caterpillars Count! Data from This Week 1!", array(Site::findByID("2")), 57, 3, "plocharczykweb");
+  email8("plocharczykweb@gmail.com", "Check Your Caterpillars Count! Data from This Week!", array(Site::findByID("2")), 57, 3, "hurlbert", true);
 ?>
