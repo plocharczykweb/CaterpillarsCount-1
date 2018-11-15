aeed0ad70e182b1685fd8581315f418618e3f551
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-14 20:20:44 -0500

    add site authority emails (email7)

--- php/emails/sundayNight.php
+++ php/emails/sundayNight.php
@@ -1,24 +1,31 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
+	
 	require_once("../orm/Site.php");
 	require_once("../orm/User.php");
+	require_once("../orm/resources/Keychain.php");
 	require_once("../orm/resources/mailing.php");
-  
+	
 	$sites = Site::findAll();
 	$dbconn = (new Keychain)->getDatabaseConnection();
+	$today = date("Y-m-d");
+	$sundayOffset = date('w', strtotime($today));
+    	$monday = date("Y-m-d", strtotime($today . " -" . (6 + $sundayOffset) . " days"));
 	for($i = 0; $i < count($sites); $i++){
-		if($sites[$i]->getActive() && $sites[$i]->getNumberOfSurveysByYear(date("Y")) <= 2){
-			$query = mysqli_query($dbconn, "SELECT COUNT(Survey.*) AS Count FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $$sites[$i]->getID() . "' AND Survey.LocalDate>'" . date("Y") . "-06-13'");
-			if(intval(mysqli_fetch_assoc($query)["Count"]) == 0){
-				$emails = $sites[$i]->getAuthorityEmails();
-				for($j = 0; $j < count($emails); $j++){
-					$firstName = "there";
-					$user = User::findByEmail();
-					if(is_object($user) && get_class($user) != "User"){
-						$firstName = $user->getFirstName();
-					}
-					email6($emails[$j], "Touching Base about " . $sites[$i]->getName(), $sites[$i]->getName());
-				}
+		$query = mysqli_query($dbconn, "SELECT COUNT(Survey.*) AS SurveyCount, COUNT(DISTINCT(Survey.UserFKOfObserver)) AS UserCount FROM Survey JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $$sites[$i]->getID() . "' AND Survey.LocalDate>='$monday'");
+		$surveyCount = intval(mysqli_fetch_assoc($query)["SurveyCount"]);
+		$userCount = intval(mysqli_fetch_assoc($query)["UserCount"]);
+		if($surveyCount > 0){
+			//site with surveys since monday email
+			$query = mysqli_query($dbconn, "SELECT SUM(ArthropodSighting.Quantity) AS ArthropodCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $$sites[$i]->getID() . "' AND Survey.LocalDate>='$monday'");
+			$arthropodCount = intval(mysqli_fetch_assoc($query)["ArthropodCount"]);
+			
+			$query = mysqli_query($dbconn, "SELECT SUM(ArthropodSighting.Quantity) AS CaterpillarCount FROM ArthropodSighting JOIN Survey ON ArthropodSighting.SurveyFK=Survey.ID JOIN Plant ON Survey.PlantFK=Plant.ID WHERE `SiteFK`='" . $$sites[$i]->getID() . "' AND Survey.LocalDate>='$monday' AND ArthropodSighting.Group='caterpillar'");
+			$caterpillarCount = intval(mysqli_fetch_assoc($query)["CaterpillarCount"]);
+			
+			$emails = $sites[$i]->getAuthorityEmails();
+			for($j = 0; $j < count($emails); $j++){
+				email7($emails[$j], "This Week at " . $sites[$i]->getName() . "...", $userCount, $surveyCount, $sites[$i]->getName(), $arthropodCount, $caterpillarCount, $sites[$i]->getID());
 			}
 		}
 	}
