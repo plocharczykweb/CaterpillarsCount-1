bebdb32e8cdbaa4be7ab0c93bcf3988e0f1d4ddf
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-15 02:00:10 -0500

    test email8 too

--- php/emails/test.php
+++ php/emails/test.php
@@ -1,10 +1,14 @@
 <?php
   header('Access-Control-Allow-Origin: *');
   
+  require_once("../orm/Site.php");
   require_once("../orm/resources/mailing.php");
   
   email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Aaron");
   email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Aaron");
   email6("plocharczykweb@gmail.com", "Touching Base about Example Site", "Example Site");
   email7("plocharczykweb@gmail.com", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
+  email8("plocharczykweb@gmail.com", "Check Your Caterpillars Count! Data from This Week 1!", array(Site::findByID("2")), 57, 3, "plocharczykweb");
+  email8("plocharczykweb@gmail.com", "Check Your Caterpillars Count! Data from This Week 2!", array(Site::findByID("2"), Site::findByID("77")), 57, 3, "plocharczykweb");
+  email8("plocharczykweb@gmail.com", "Check Your Caterpillars Count! Data from This Week 3!", array(Site::findByID("2"), Site::findByID("77"), Site::findByID("60")), 57, 3, "plocharczykweb");
 ?>
