40cb6c66870cf0f44758493e8f48b29b2374891c
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-14 19:21:23 -0500

    test email6 too

--- php/emails/test.php
+++ php/emails/test.php
@@ -5,4 +5,5 @@
   
   email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Aaron");
   email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Aaron");
+  email6("plocharczykweb@gmail.com", "Touching Base about Example Site", "Example Site");
 ?>
