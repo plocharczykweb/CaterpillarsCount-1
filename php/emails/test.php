44d57ec0b6482664807ae8eeec25e090e36ed10e
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-15 03:41:16 -0500

    send to Allen

--- php/emails/test.php
+++ php/emails/test.php
@@ -4,9 +4,9 @@
   require_once("../orm/Site.php");
   require_once("../orm/resources/mailing.php");
   
-  email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Allen");
-  email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
-  email6("plocharczykweb@gmail.com", "Touching Base about Example Site", "Example Site");
-  email7("plocharczykweb@gmail.com", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
-  email8("plocharczykweb@gmail.com", "Check Your Caterpillars Count! Data from This Week!", array(Site::findByID("2")), 57, 3, "hurlbert", true);
+  email4("hurlbert@bio.unc.edu", "The Caterpillars Count! Season Has Begun!", "Allen");
+  email5("hurlbert@bio.unc.edu", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
+  email6("hurlbert@bio.unc.edu", "Touching Base about Example Site", "Example Site");
+  email7("hurlbert@bio.unc.edu", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
+  email8("hurlbert@bio.unc.edu", "Check Your Caterpillars Count! Data from This Week!", array(Site::findByID("2")), 57, 3, "hurlbert", true);
 ?>
