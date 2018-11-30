af800a755cd33dfdb1f1521b3c1e949653a8e779
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-30 15:27:34 -0500

    cross test everything

--- php/emails/test.php
+++ php/emails/test.php
@@ -6,10 +6,14 @@
   
   email3("aaronplo@live.unc.edu", "Preparing for a new Caterpillars Count! Season", "Example Site", "10", "2", "2", "21", "15", "427", "14", "1932", "187", "9.27");
   email3("plocharczykweb@gmail.com", "Preparing for a new Caterpillars Count! Season", "Example Site", "10", "2", "2", "21", "15", "427", "14", "1932", "187", "9.27");
-  //email4("aaronplo@live.unc.edu", "The Caterpillars Count! Season Has Begun!", "Allen");
-  //email5("aaronplo@live.unc.edu", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
-  //email6("aaronplo@live.unc.edu", "Caterpillars Count! at Example Site", "Example Site");
-  //email7("aaronplo@live.unc.edu", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "beetle", "22", "", "0", "2");
+  email4("aaronplo@live.unc.edu", "The Caterpillars Count! Season Has Begun!", "Allen");
+  email4("plocharczykweb@gmail.com", "The Caterpillars Count! Season Has Begun!", "Allen");
+  email5("aaronplo@live.unc.edu", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
+  email5("plocharczykweb@gmail.com", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
+  email6("aaronplo@live.unc.edu", "Caterpillars Count! at Example Site", "Example Site");
+  email6("plocharczykweb@gmail.com", "Caterpillars Count! at Example Site", "Example Site");
+  email7("aaronplo@live.unc.edu", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "beetle", "22", "", "0", "2");
+  email7("plocharczykweb@gmail.com", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "beetle", "22", "", "0", "2");
   email8("aaronplo@live.unc.edu", "Your Caterpillars Count! weekly summary", array(Site::findByID("2")), 57, 3, "hurlbert", true);
   email8("plocharczykweb@gmail.com", "Your Caterpillars Count! weekly summary", array(Site::findByID("2")), 57, 3, "hurlbert", true);
 ?>
