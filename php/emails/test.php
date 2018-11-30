134f29202cf5a1789b91f31edfee0a2f3c6b6e24
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-29 21:59:14 -0500

    cross test email6

--- php/emails/test.php
+++ php/emails/test.php
@@ -7,7 +7,7 @@
   //email4("aaronplo@live.unc.edu", "The Caterpillars Count! Season Has Begun!", "Allen");
   //email5("aaronplo@live.unc.edu", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
   email6("aaronplo@live.unc.edu", "Touching Base about Example Site", "Example Site");
+  email6("plocharczykweb@gmail.com", "Touching Base about Example Site", "Example Site");
   //email7("aaronplo@live.unc.edu", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
   //email8("aaronplo@live.unc.edu", "Check Your Caterpillars Count! Data from This Week!", array(Site::findByID("2")), 57, 3, "hurlbert", true);
-  mailchimp("aaronplo@live.unc.edu", "mailchimp test");
 ?>
