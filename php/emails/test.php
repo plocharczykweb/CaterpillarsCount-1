0547e5dd2576987704cf2bd2df3682502a468188
caterpillarscountdev <35781616+caterpillarscountdev@users.noreply.github.com>
2018-11-29 20:03:55 -0500

    add mailchimp test

--- php/emails/test.php
+++ php/emails/test.php
@@ -4,9 +4,10 @@
   require_once("../orm/Site.php");
   require_once("../orm/resources/mailing.php");
   
-  email4("aaronplo@live.unc.edu", "The Caterpillars Count! Season Has Begun!", "Allen");
-  email5("aaronplo@live.unc.edu", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
+  //email4("aaronplo@live.unc.edu", "The Caterpillars Count! Season Has Begun!", "Allen");
+  //email5("aaronplo@live.unc.edu", "Need Help Submitting Caterpillars Count! Surveys?", "Allen");
   email6("aaronplo@live.unc.edu", "Touching Base about Example Site", "Example Site");
-  email7("aaronplo@live.unc.edu", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
-  email8("aaronplo@live.unc.edu", "Check Your Caterpillars Count! Data from This Week!", array(Site::findByID("2")), 57, 3, "hurlbert", true);
+  //email7("aaronplo@live.unc.edu", "This Week at Example Site...", "17", "85", "Example Site", "248", "19", "2");
+  //email8("aaronplo@live.unc.edu", "Check Your Caterpillars Count! Data from This Week!", array(Site::findByID("2")), 57, 3, "hurlbert", true);
+  mailchimp("aaronplo@live.unc.edu", "mailchimp test");
 ?>
